a1507eab5261f15ededc2310fa1827a202215d2e
facundoLopezBruno <facundo.lopezbruno@mercadolibre.com>
2024-04-10 18:01:28 -0300

    practicas vivo POO P2

---
+++ 1. Modulo Java/numeros_excepciones/src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 300;
+
+        try {
+            double division = b / a;
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error" + e.getMessage());
+        } finally {
+            System.out.println("Se ha finalizado la ejecución");
+        }
+
+        //segunda manejo execcion
+        try{
+            int res = b/a;
+        }
+        catch (ArithmeticException e){
+            throw new IllegalArgumentException("No se permite el 0 como divisor");
+        }
+        finally {
+            System.out.println("Programa finalizado 2da excepcion");
+        }
+    }
+}
+
