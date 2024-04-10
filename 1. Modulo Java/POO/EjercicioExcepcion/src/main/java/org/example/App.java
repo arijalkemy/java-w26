6ad2c8330236f49e39028bc55403038013dd2df0
gonzalopalomo <gonzalo.palomo@mercadolibre.com>
2024-04-10 14:46:31 -0300

    Se agrego los bloques try catch

---
+++ 1. Modulo Java/POO/EjercicioExcepcion/src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        //Mensaje final
+        String mensajeFinal = "Este es el último mensaje";
+
+        //Código que arroja excepción
+        try
+        {
+            int[] numeros = new int[5];
+            numeros[5] = 10;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println(mensajeFinal);
+        }
+
+    }
+}
