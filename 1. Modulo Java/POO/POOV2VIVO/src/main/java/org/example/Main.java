809a875878da1da32481541d93fb96805d7ccbb2
FranciscoCardosoRodriguez <franciscocardosorodriguez@gmail.com>
2024-04-10 16:57:20 -0600

    Organización de repo

---
+++ 1. Modulo Java/POO/POOV2VIVO/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        PracticaExcepciones pe = new PracticaExcepciones();
+        try {
+            pe.calcularCociente();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ 1. Modulo Java/POO/POOV2VIVO/src/main/java/org/example/PracticaExcepciones.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class PracticaExcepciones {
+    int a = 0;
+    int b = 300;
+
+    public void calcularCociente() {
+        try {
+            int resultado = b / a;
+            System.out.println("El resultado es: " + resultado);
+        } catch (ArithmeticException e) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        } finally {
+            System.out.println("Programa finalizado");
+        }
+    }
+
+
+}
