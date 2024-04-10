82e0671c6a641d981f1250b7191a6ce3c98fa212
John Stiven Garcia Vargas <johnstiven.garciavargas@mercadolibre.com.co>
2024-04-10 14:50:28 -0500

    Agrego la solucion del ejercicio practico de POO 2

---
+++ 1. Modulo Java/POO P2/ejercicio_practico/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Mensaje final
+        String mensajeFinal = "Este es el último mensaje";
+
+        try{
+            //Código que arroja excepción
+            int[] numeros = new int[5];
+            numeros[5] = 10;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println(mensajeFinal);
+
+    }
+}
