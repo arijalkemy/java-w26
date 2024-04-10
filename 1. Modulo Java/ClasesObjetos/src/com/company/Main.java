54feabef4ca47449218dda4f6b0c69548d78bc84
levianfrancisco <levianlastra@gmail.com>
2024-04-10 10:49:19 -0300

    Ejercicio del video de clases y objetos

---
+++ 1. Modulo Java/ClasesObjetos/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Libro libro = new Libro("Harry Potter", "J. K. Rowling", 20);
+        System.out.println(libro.mostrarLibro());
+        System.out.println("La cantidad de ejemplares para este libroes de: " + libro.cantidadDeEjemplares());
+    }
+}
