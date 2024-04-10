a97fd524c8234739ebaa17c03d79b8aff63052a2
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-04-10 09:34:46 -0600

    Ejercicio 5

--- 1. Modulo Java/POO1-EjerciciosPracticos/src/Main.java
+++ 1. Modulo Java/POO1-EjerciciosPracticos/src/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         Persona personaVacio = new Persona();
-        Persona personaSinCar = new Persona("Nombre", 20, "1234567");
+        Persona personaSinCar = new Persona("Persona", 20, "1234567");
         Persona personaCompleta = new Persona("Nombre", 21, "4567890", 80, 19.0);
 
         Persona personaSinDni = new Persona("Nombre", 20);
 
+        System.out.println(personaSinDni.toString());
     }
 }
--- 1. Modulo Java/POO1-EjerciciosPracticos/src/Persona.java
+++ 1. Modulo Java/POO1-EjerciciosPracticos/src/Persona.java
@@ -26,4 +26,47 @@ public class Persona {
         this.peso = peso;
         this.altura = altura;
     }
+
+    public int calcularIMC(){
+        double imc = this.peso / Math.pow(this.altura, 2);
+
+        if (imc < 20){
+            return -1;
+        } else if (imc >= 20 && imc <= 25) {
+            return 0;
+        }
+
+        return 1;
+    }
+
+    public boolean esMayorDeEdad(){
+        if (this.edad >= 18){
+            return true;
+        }
+        return false;
+    }
+
+
+    public String toString() {
+
+        if (this.nombre == null){
+            return "Esta persona no tiene informacion";
+        }
+
+        if (this.peso != 0 && this.altura != 0){
+            return "Nombre: " + this.nombre + "" +
+                "\nEdad: "+ this.edad +
+                "\nDNI: " + this.dni +
+                "\nAltura: " + this.altura +
+                "\nPeso: " + this.peso +
+                "\nIMC: " + this.calcularIMC() +
+                "\nMayor de Edad: " + this.esMayorDeEdad();
+        }else {
+            return "Nombre: " + this.nombre + "" +
+                    "\nEdad: "+ this.edad +
+                    "\nDNI: " + this.dni;
+        }
+
+
+    }
 }
