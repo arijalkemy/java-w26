63adc46fc7d4da18ac4b267bcb26134e17a2c142
Franco Matias Nicotra <franco.nicotra@mercadolibre.com>
2024-04-10 11:34:37 -0300

    POO P1 - Ejercicio 2

--- 1. Modulo Java/POO-P1/src/Persona.java
+++ 1. Modulo Java/POO-P1/src/Persona.java
@@ -4,4 +4,21 @@ public class Persona {
     private String dni;
     private double peso;
     private double altura;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona(String nombre, int edad, String dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
 }
