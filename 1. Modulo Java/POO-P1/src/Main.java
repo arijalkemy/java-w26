4b266906d6f73a9dd239c8de26c23a7fa2243deb
Franco Matias Nicotra <franco.nicotra@mercadolibre.com>
2024-04-10 12:00:55 -0300

    POO P1 - Ejercicio 4

--- 1. Modulo Java/POO-P1/src/Main.java
+++ 1. Modulo Java/POO-P1/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Se instancian personas con distintos tipos de constructores
+        Persona personaSinConstructor = new Persona();
+        Persona personaConTresParametros = new Persona("Franco", 25, "99999999");
+        Persona personaCompleta = new Persona("Franco", 25, "99999999", 10.0, 100.0);
+
+        //No se puede construir la persona a continuacion ya que no se creo un constructor que acepte esos parametros en la clase persona
+        //Persona personaConDosParametros = new Persona("Franco", 25);
     }
 }
