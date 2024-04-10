f626783f0beb6bb62665d174f44d3ba68ea39dfc
jbendezugonz_meli <jonathanleonel.bendezugonzalez@mercadolibre.com>
2024-04-10 13:01:55 -0300

    Add(JAVA): proyecto 05

---
+++ 1. Modulo Java/05_imc/src/main/java/bootcamp/bendezujonathan/App.java
@@ -0,0 +1,13 @@
+package bootcamp.bendezujonathan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
