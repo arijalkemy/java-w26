bfe8e769dc207cc6938fd7e63408edec82e7109f
Edwin Andres Villarraga Ardila <edwin.villarragaardila@mercadolibre.com.co>
2024-04-10 16:02:29 -0500

    FEAT: ejericicios de herencia y manejo de excepciones

---
+++ 1. Modulo Java/Exceptions/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        PracticaExcepciones pe = new PracticaExcepciones(10, 5);
+        pe.calcular();
+    }
+}
---
+++ 1. Modulo Java/Exceptions/PracticaExcepciones.java
@@ -0,0 +1,22 @@
+public class PracticaExcepciones {
+    private int a;
+    private int b;
+
+    public PracticaExcepciones(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void calcular() {
+        try {
+            if (b == 0) {
+                throw new IllegalArgumentException("No se puede dividir por cero");
+            }
+            System.out.println(a / b);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Programa finalizado");
+        }
+    }
+}
