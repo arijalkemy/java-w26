aff18f1d1b389dc708c371747563ad97b9f6cb7c
gonzalopalomo <gonzalo.palomo@mercadolibre.com>
2024-04-10 13:06:08 -0300

    Se agrego try catch y se subio los datos de la clase persona

--- 1. Modulo Java/POO/Practica Integradora Poo/src/main/java/org/example/Persona.java
+++ 1. Modulo Java/POO/Practica Integradora Poo/src/main/java/org/example/Persona.java
@@ -1,2 +1,73 @@
-package org.example;public class Persona {
+package org.example;
+
+// Ejercicio 1
+public class Persona {
+    String nombre;
+    int edad;
+    String dni;
+    double peso;
+    double altura;
+
+    // Constructor sin parametros ejercicio 2
+    public Persona() {
+    }
+
+    // ejercicio 2
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    // Constructor con todos los parametros ejercicio 2
+    public Persona(String nombre, int edad, String dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    // Ejercicio 5
+    public int calcularIMC()
+    {
+        try{
+            double resultado = peso/(Math.pow(this.altura,2));
+            if(resultado<20)
+            {
+                return -1;
+            }
+            else if(resultado>=20&& resultado<=25)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        catch (ArithmeticException e)
+        {
+            throw new Error(e.getMessage());
+        }
+    }
+    // Ejercicio 5
+
+    public boolean esMayorDeEdad()
+    {
+        if(this.edad>=18)
+        {
+            return true;
+        }
+        return false;
+    }
+    // Ejercicio 5
+
+    public String toString()
+    {
+        return "Nombre: " + this.nombre + " Edad: " + this.edad + " Dni: " + this.dni + " Peso: " + this.peso + " Altura: " + this.altura;
+    }
+
+
+
 }
