cb1c0e6734104f19234cfe724239de9a5df48725
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-04-10 09:09:01 -0600

    Ejercicio 2

--- 1. Modulo Java/POO1-EjerciciosPracticos/src/Persona.java
+++ 1. Modulo Java/POO1-EjerciciosPracticos/src/Persona.java
@@ -5,5 +5,20 @@ public class Persona {
     double peso;
     double altura;
 
+    public Persona() {
+    }
 
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona(String nombre, int edad, String dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
 }
