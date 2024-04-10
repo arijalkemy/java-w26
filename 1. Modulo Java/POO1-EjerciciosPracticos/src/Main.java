188a15c6bb835566dc0fe75fb83aa739b58b2fab
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-04-10 09:39:54 -0600

    Ejercicio 6

--- 1. Modulo Java/POO1-EjerciciosPracticos/src/Main.java
+++ 1. Modulo Java/POO1-EjerciciosPracticos/src/Main.java
@@ -5,10 +5,28 @@ public class Main {
 
         Persona personaVacio = new Persona();
         Persona personaSinCar = new Persona("Persona", 20, "1234567");
-        Persona personaCompleta = new Persona("Nombre", 21, "4567890", 80, 19.0);
+        Persona personaCompleta = new Persona("Nombre", 18, "4567890", 80, 19.0);
 
-        Persona personaSinDni = new Persona("Nombre", 20);
 
-        System.out.println(personaSinDni.toString());
+
+        Persona personaSinDni = new Persona("Nombre", 17);
+
+
+        System.out.println("Persona sin Información:");
+        System.out.println(personaVacio.toString() + "\n");
+        System.out.println("Persona sin Caracteristicas Fisicas:");
+        System.out.println(personaSinCar.toString() + "\n");
+        System.out.println("Persona Con toda la Información:");
+        System.out.println(personaCompleta.toString() + "\n");
+        System.out.println("Persona sin DNI:");
+        System.out.println(personaSinDni.toString() + "\n");
+
+
+        System.out.println("Calculando desde main a persona completa");
+        System.out.println("IMC:" + personaCompleta.calcularIMC());
+        System.out.println("Es Mayor de Edad:" + personaCompleta.esMayorDeEdad());
+
+
+
     }
 }
