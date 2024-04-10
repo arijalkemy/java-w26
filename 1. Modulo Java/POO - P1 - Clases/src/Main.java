bcbd38d144f8fd3da37c970a444e894e65c5fbf6
JonathanFPH <jonathanfrancisco.perezhernandez@mercadolibre.com.mx>
2024-04-10 00:11:14 -0600

    Subiendo ejercicio de POO - P1 - Clases

---
+++ 1. Modulo Java/POO - P1 - Clases/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Libro libro = new Libro("Harry Potter", "J. K. Rowling", 20);
+        System.out.println(libro.mostrarLibro());
+        System.out.println("La cantidad de ejemplares para este libro es de: " + libro.cantidadDeEjemplares());
+    }
+}
