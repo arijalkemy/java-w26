b3f11b5efda43e47afbe22a5c39fdb68b608a9a0
mecraviotto <mateo.craviotto@mercadolibre.com>
2024-04-10 11:27:37 -0300

    Agrego ejercicios 3 y 4 POO P1

--- 1. Modulo Java/POO1/src/main/java/org/example/Main.java
+++ 1. Modulo Java/POO1/src/main/java/org/example/Main.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Persona personaVacia = new Persona();
+        Persona personaAMedias = new Persona("Mateo", 23, "12312345");
+        Persona personaCompleta = new Persona("Exequiel", 23, "12123123", 75, 1.75);
+        // Persona personaSinEdad = new Persona("Juan", 25);
+        // Esto no funciona porque no existe un constructor para estos parámetros
     }
 }
 
