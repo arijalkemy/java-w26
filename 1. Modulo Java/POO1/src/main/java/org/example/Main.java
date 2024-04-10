2a0968afbcca5b3f4d5403ec9d649f0e735e3457
mecraviotto <mateo.craviotto@mercadolibre.com>
2024-04-10 11:57:40 -0300

    Agrego ejercicio 6 POO P1

--- 1. Modulo Java/POO1/src/main/java/org/example/Main.java
+++ 1. Modulo Java/POO1/src/main/java/org/example/Main.java
@@ -8,6 +8,12 @@ public class Main {
         Persona personaCompleta = new Persona("Exequiel", 23, "12123123", 75, 1.75);
         // Persona personaSinEdad = new Persona("Juan", 25);
         // Esto no funciona porque no existe un constructor para estos parámetros
+        double imc = personaCompleta.calcularIMC();
+
+        System.out.println(personaCompleta.toString());
+        System.out.println("Su IMC es: " + imc);
+        String mensajeEdad = personaCompleta.esMayorDeEdad() ? "Es mayor de edad" : "Es menor de edad";
+        System.out.println(mensajeEdad);
     }
 }
 
