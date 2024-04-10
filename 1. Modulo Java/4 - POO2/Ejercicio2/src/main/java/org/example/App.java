c7302440591518c30f1beaf13543cd9ec746f2c6
joaquin-oldani <joaquin.oldani@mercadolibre.com>
2024-04-10 19:13:50 -0300

    POO2: ejercicio 2 parte 4 lista

--- 1. Modulo Java/4 - POO2/Ejercicio2/src/main/java/org/example/App.java
+++ 1. Modulo Java/4 - POO2/Ejercicio2/src/main/java/org/example/App.java
@@ -1,13 +1,40 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.Random;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ArrayList<Producto> productosP = new ArrayList<Producto>();
+        productosP.add(new Perecedero("Pata muslo", 10.2, 3));
+        productosP.add(new Perecedero("Huevos", 6.3, 2));
+        productosP.add(new Perecedero("Hamburguesa", 8.4, 1));
+        productosP.add(new Perecedero("Milanesa", 12.3, 5));
+        productosP.add(new Perecedero("Pechuga", 9.5, 7));
+
+        ArrayList<Producto> productosNP = new ArrayList<Producto>();
+        productosNP.add(new NoPerecedero("Choclo", 7.4, "A"));
+        productosNP.add(new NoPerecedero("Lentejas", 6.9, "B"));
+        productosNP.add(new NoPerecedero("Arvejas", 5.0, "C"));
+        productosNP.add(new NoPerecedero("Jardinera", 8.2, "A"));
+        productosNP.add(new NoPerecedero("Garbanzo", 7.7, "B"));
+
+        // Se utiliza Random para generar una cantidad aleatoria de productos y obtener un precio
+        Random random = new Random();
+
+        double totalP = 0;
+        for (Producto pr : productosP){
+            totalP =+ pr.calcular(random.nextInt(10) + 1);
+        }
+
+        double totalNP = 0;
+        for (Producto pr : productosNP){
+            totalNP =+ pr.calcular(random.nextInt(10) + 1);
+        }
+
+        System.out.println("Total acumulado de productos Perecederos: " + totalP);
+        System.out.println("Total acumulado de productos No Perecederos: " + totalNP);
     }
 }
---
+++ 1. Modulo Java/4 - POO2/Ejercicio2/src/main/java/org/example/Perecedero.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Perecedero extends Producto{
+    int diasPorCaducar;
+
+    public int getDiasPorCaducar() {
+        return diasPorCaducar;
+    }
+
+    public void setDiasPorCaducar(int diasPorCaducar) {
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    public Perecedero(String nombre, double precio, int diasPorCaducar) {
+        super(nombre, precio);
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    @Override
+    public String toString() {
+        return "Perecedero{" +
+                "diasPorCaducar=" + diasPorCaducar +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    @Override
+    public double calcular(int cantidadDeProductos){
+        switch (this.diasPorCaducar){
+            case 1:
+                return super.calcular(cantidadDeProductos) / 4;
+
+            case 2:
+                return super.calcular(cantidadDeProductos) / 3;
+
+            case 3:
+                return super.calcular(cantidadDeProductos) / 2;
+
+            default:
+                return super.calcular(cantidadDeProductos);
+        }
+    }
+}
