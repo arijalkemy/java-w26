e8875b7bd64abcf8e6f4ff07a3c390dd75465b8c
joaquin-oldani <joaquin.oldani@mercadolibre.com>
2024-04-10 10:36:28 -0300

    POO: ejercicio 2

--- 1. Modulo Java/3 - POO/Persona.java
+++ 1. Modulo Java/3 - POO/Persona.java
@@ -4,4 +4,22 @@ public class Persona {
     String dni;
     float peso;
     float altura;
+
+    public Persona(){
+        
+    }
+
+    public Persona(String nombre, int edad, String dni){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona(String nombre, int edad, String dni, float peso, float altura){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
 }
