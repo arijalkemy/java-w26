f26aea0d04484a026a6aeb7516ce60971f389ca4
John Stiven Garcia Vargas <johnstiven.garciavargas@mercadolibre.com.co>
2024-04-10 12:18:18 -0500

    Agrego la solucion de los ejercicios practivos de Intro Java P1

---
+++ 1. Modulo Java/introduccion Java P1/ejercicios_practicos/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public void ejercicioUno(){
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean valor= false;
+        String sueldo = "45857.90";
+        String nombre = "Julián";
+    }
+
+    public void ejercicioDos(){
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licencia_de_conducir;
+        double estatura_de_la_persona;
+        int cantidad_de_hijos;
+    }
+}
