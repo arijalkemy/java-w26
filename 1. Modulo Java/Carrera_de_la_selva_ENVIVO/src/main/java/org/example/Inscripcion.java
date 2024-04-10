741f0c8e3e2e4b4d8ffe6d29ad21dbce3d9c8fe4
dbellolopez <tu_email@ejemplo.com>
2024-04-10 12:18:15 -0500

    Ejercicio temperaturaas EN VIVO

---
+++ 1. Modulo Java/Carrera_de_la_selva_ENVIVO/src/main/java/org/example/Inscripcion.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Inscripcion {
+    private static int contadorInscripcion = 1;
+    private int numeroInscripcion;
+    private Categoria categoria;
+    private Participante participante;
+    private int monto;
+
+    public Inscripcion(Categoria categoria, Participante participante) {
+        this.numeroInscripcion = contadorInscripcion++;
+        this.categoria = categoria;
+        this.participante = participante;
+        this.monto = calcularMonto();
+    }
+
+    private int calcularMonto() {
+        switch (categoria.getId()) {
+            case 1: // Circuito chico
+                return participante.getEdad() < 18 ? 1300 : 1500;
+            case 2: // Circuito medio
+                return participante.getEdad() < 18 ? 2000 : 2300;
+            case 3: // Circuito avanzado
+                if (participante.getEdad() < 18) {
+                    throw new IllegalArgumentException("No se permite inscripciones a menores de 18 años en el Circuito Avanzado.");
+                }
+                return 2800;
+            default:
+                return 0;
+        }
+    }
+
+    // Getters y setters
+    public int getNumeroInscripcion() {
+        return numeroInscripcion;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public Participante getParticipante() {
+        return participante;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+}
+
