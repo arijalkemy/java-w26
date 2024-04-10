4bb43ea09bacd2f670ecdaee299f66ee916bfe88
Lautaro Oleastro <lautaro.oleastro@mercadolibre.com>
2024-04-10 12:47:31 -0300

    Agregando ejercicio de calculo de IMC de persona (POO P1)

---
+++ 1. Modulo Java/POO P1 - Calculo IMC/CalculoIMC/src/main/java/org/example/App.java
@@ -0,0 +1,37 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Persona personaSinParametros = new Persona();
+        Persona personaTresParametros = new Persona("Lautaro Oleastro", 20, "40989847");
+        Persona personaCincoParametros = new Persona("Lautaro Oleastro", 20, "40989847", 72.0,1.72);
+
+        personaCincoParametros.calcularIMC();
+
+        String estadoDePeso = "";
+        switch (personaCincoParametros.calcularIMC()){
+            case -1:
+                estadoDePeso = "Bajo peso.";
+                break;
+            case 0:
+                estadoDePeso = "Peso saludale.";
+                break;
+            case 1:
+                estadoDePeso = "Sobrepeso.";
+        }
+
+        System.out.println(personaCincoParametros.toString());
+        System.out.println(estadoDePeso);
+        if (personaCincoParametros.esMayorDeEdad()) {
+            System.out.println("Es mayor de edad.");
+        } else {
+            System.out.println("Es menor de edad.");
+        }
+    }
+}
---
+++ 1. Modulo Java/POO P1 - Calculo IMC/CalculoIMC/src/main/java/org/example/Persona.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String dni;
+    private double peso;
+    private double altura;
+
+    public Persona() {
+
+    }
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona(String nombre, int edad, String dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int calcularIMC() {
+        double resultado = this.peso / (this.altura * this.altura);
+        if (resultado < 20) {
+            return -1;
+        } else if (resultado > 25) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public boolean esMayorDeEdad() {
+        return (this.edad >= 18);
+    }
+
+    @Override
+    public String toString() {
+        return " - Datos de la persona - \n" +
+                "nombre: " + nombre + '\n' +
+                "edad: " + edad + '\n' +
+                "dni: " + dni + '\n' +
+                "peso: " + peso + '\n' +
+                "altura: " + altura;
+    }
+}
