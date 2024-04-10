04e8938e5abd2c1204a83ccf8e68244df2fcf372
ignruiz <ignacio.iruiz@mercadolibre.com>
2024-04-10 14:48:14 -0300

    Ejercicio practica POO-P2

---
+++ 1. Modulo Java/POO-P2/src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Mensaje final
+        String mensajeFinal = "Este es el último mensaje";
+
+        //Código que arroja excepción
+        int[] numeros = new int[5];
+        try{
+            numeros[5] = 10;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println(mensajeFinal);
+        }
+
+    }
+}
