cb1c792f8909cb8b61a29159259032b55ab6e034
Brian Zarate <brian.zarate@mercadolibre.com>
2024-04-11 16:30:26 -0300

    ejercicios introduccion a java parte 1

---
+++ 1. Modulo Java/introduccion-a-java/p1/src/main/java/org/ejercicio1/App.java
@@ -0,0 +1,17 @@
+package org.ejercicio1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean booleano = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";
+    }
+}
---
+++ 1. Modulo Java/introduccion-a-java/p1/src/main/java/org/ejercicio2/App.java
@@ -0,0 +1,25 @@
+package org.ejercicio2;
+
+public class App {
+    public static void main(String[] args) {
+        /**
+         * String 1nombre;
+         * int edad;
+         * double $ueldo;
+         * String @pellido;
+         * String direccion;
+         * boolean licencia_de_conducir;
+         * double estatura de la persona;
+         * int cantidad-de-hijos;
+         */
+
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licencia_de_conducir;
+        double estatura_de_la_persona;
+        int cantidad_de_hijos;
+    }
+}
