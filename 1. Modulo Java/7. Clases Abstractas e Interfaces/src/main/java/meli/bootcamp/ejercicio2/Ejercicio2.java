51e74e25e866d65ad47dec24a14a66704b533abc
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-04-11 13:06:24 -0300

    agregar clase ejecutable de prueba

--- 1. Modulo Java/7. Clases Abstractas e Interfaces/src/main/java/meli/bootcamp/ejercicio2/Ejercicio2.java
+++ 1. Modulo Java/7. Clases Abstractas e Interfaces/src/main/java/meli/bootcamp/ejercicio2/Ejercicio2.java
@@ -1,7 +1,33 @@
 package meli.bootcamp.ejercicio2;
 
+import meli.bootcamp.ejercicio2.documentos.Documento;
+import meli.bootcamp.ejercicio2.documentos.impl.Curriculum;
+import meli.bootcamp.ejercicio2.documentos.impl.Informe;
+import meli.bootcamp.ejercicio2.documentos.impl.LibroEnPdf;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Ejercicio2 {
   public static void main(String[] args) {
+    LibroEnPdf libro = new LibroEnPdf(
+        99, "Antoine de Saint-Exupéry", "El principito", "Infantil"
+    );
 
+    Informe informe = new Informe(
+    10, "Juan Perez", "Maria Gomez", "Informe de ventas"
+    );
+
+    Curriculum curriculum = new Curriculum(
+    "Juan", "Perez", "Buenos Aires", "1234", "juan@gmail.com"
+    );
+
+    curriculum.agregarEducacion("UTN - Ingeniería en Sistemas");
+    curriculum.agregarExperiencia("MELI - Software Developer");
+    curriculum.agregarHabilidad("Java");
+
+    List<Documento> documentosParaImprimir = Arrays.asList(libro, informe, curriculum);
+
+    documentosParaImprimir.forEach(Documento::imprimir);
   }
 }
