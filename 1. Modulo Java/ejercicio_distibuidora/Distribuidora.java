9593e4e80964fc936721b7dfc00c55556d600bd4
agustin.chiaravalli@mercadolibre.com <164513438+AaChiaravalli@users.noreply.github.com>
2024-04-10 21:34:45 -0300

    Actualización de la class Distribuidora

--- 1. Modulo Java/ejercicio_distibuidora/Distribuidora.java
+++ 1. Modulo Java/ejercicio_distibuidora/Distribuidora.java
@@ -13,15 +13,30 @@ public class Distribuidora {
         productosPerecederos[2] = new Perecedero("Melon",11.1,3);
         productosPerecederos[3] = new Perecedero("Sandia",200,4);
         productosPerecederos[4] = new Perecedero("Uva",33,10);
-        System.out.println(productosPerecederos.length);
+
+        productosNoPerecederos[0] = new NoPerecedero("Fideos",2500,"x");
+        productosNoPerecederos[1] = new NoPerecedero("Dulce de leche",32.30,"x");
+        productosNoPerecederos[2] = new NoPerecedero("Arroz",94.5,"m");
+        productosNoPerecederos[3] = new NoPerecedero("Lata de atún",222,"l");
+        productosNoPerecederos[4] = new NoPerecedero("Rebozador",332,"p");
 
         double total = 0;
+        
         for (int i = 0; i<5;i++){
             System.out.println(productosPerecederos[i].toString());
             System.out.println("Ingrese la cantidad: ");
             int cantidad = input.nextInt();
             total += productosPerecederos[i].calcular(cantidad);
         }
+
+        for (int i = 0; i<5;i++){
+            System.out.println(productosNoPerecederos[i].toString());
+            System.out.println("Ingrese la cantidad: ");
+            int cantidad = input.nextInt();
+            total += productosNoPerecederos[i].calcular(cantidad);
+        }
+
+
         System.out.println(total);
     }
 }
