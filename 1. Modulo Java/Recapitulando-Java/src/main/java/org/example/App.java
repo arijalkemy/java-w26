39cb70d08f7a56f7e4cf87a2652d7473f684a7d7
Franco Matias Nicotra <franco.nicotra@mercadolibre.com>
2024-04-11 17:11:15 -0300

    Ejercicio 2

--- 1. Modulo Java/Recapitulando-Java/src/main/java/org/example/App.java
+++ 1. Modulo Java/Recapitulando-Java/src/main/java/org/example/App.java
@@ -1,13 +1,42 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Vehiculo fiesta = new Vehiculo("fiesta", "ford", 1000.0);
+        Vehiculo focus = new Vehiculo("focus", "ford", 1200.0);
+        Vehiculo explorer = new Vehiculo("explorer", "ford", 2500.0);
+        Vehiculo uno = new Vehiculo("uno", "ford", 500.0);
+        Vehiculo cronos = new Vehiculo("cronos", "ford", 1000.0);
+        Vehiculo torino = new Vehiculo("torino", "ford", 1250.0);
+        Vehiculo aveo = new Vehiculo("aveo", "ford", 1250.0);
+        Vehiculo spin = new Vehiculo("spin", "ford", 2500.0);
+        Vehiculo corola = new Vehiculo("corola", "ford", 1200.0);
+        Vehiculo fortuner = new Vehiculo("fortuner", "ford", 3000.0);
+        Vehiculo logan = new Vehiculo("logan", "ford", 950.0);
+
+        List<Vehiculo> vehiculos = new ArrayList<>();
+        vehiculos.add(fiesta);
+        vehiculos.add(focus);
+        vehiculos.add(explorer);
+        vehiculos.add(uno);
+        vehiculos.add(cronos);
+        vehiculos.add(torino);
+        vehiculos.add(aveo);
+        vehiculos.add(spin);
+        vehiculos.add(corola);
+        vehiculos.add(fortuner);
+        vehiculos.add(logan);
+
+        
+
     }
 }
