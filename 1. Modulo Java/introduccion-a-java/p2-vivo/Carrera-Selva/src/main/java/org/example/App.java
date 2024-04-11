24edc524876f45377d9c20c8bcb29f61c5cd0641
Brian Zarate <brian.zarate@mercadolibre.com>
2024-04-11 16:51:18 -0300

    agregando nueva estructura de carpetas

---
+++ 1. Modulo Java/introduccion-a-java/p2-vivo/Carrera-Selva/src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App
+{
+    private static final List<String> circuitos = new ArrayList<>();
+
+
+
+    public static void main( String[] args )
+    {
+        //agregando circuitos
+        circuitos.add("Avanzado");
+        circuitos.add("Medio");
+        circuitos.add("Chico");
+
+
+
+
+
+
+
+    }
+}
