ecf7c8d987640e1ca6068e25d295238637f37d1b
JonathanFPH <jonathanfrancisco.perezhernandez@mercadolibre.com.mx>
2024-04-10 18:29:21 -0600

    Ejercicio POO - P2 - Herencia Polimorfismo y Encapsulamiento

---
+++ 1. Modulo Java/POO - P2 - Herencia Polimorfismo y Encapsulamiento/src/bootcamp/Main.java
@@ -0,0 +1,17 @@
+package bootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal perro = new Perro("Perro", "Tito");
+        perro.mostrarEspecie();
+        perro.hacerSonido();
+
+        Gato gato = new Gato("Gato", "Poker");
+        gato.mostrarEspecie();
+        gato.hacerSonido();
+
+        Animal animal = perro;
+        animal.mostrarEspecie();
+        animal.hacerSonido();
+    }
+}
