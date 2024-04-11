f659c2da6329d1a9b7e3cdd67e46997023df7b06
Nicolas Hoyos <nicolas.hoyos@mercadolibre.com.co>
2024-04-10 21:08:56 -0500

    Additional exercices part 2

---
+++ 1. Modulo Java/0_Ejercicios_Adicionales/2_Ejercicio_Facturas.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int serviciosCli[] = {1,1,2,2,2,1,2}; //vector de 7 posiciones con tipos de servicios
+        double totalFactura;
+
+        for (int i = 0; i < serviciosCli.length;i++) {
+            if (serviciosCli[i] == 1) {
+                totalFactura = 1500.0;
+                System.out.println ("El tipo de servicio es: " + serviciosCli[i]);
+                System.out.println ("El monto de la factura es de: " + totalFactura);
+            }
+            else {
+                totalFactura = 2200.0;
+                System.out.println ("El tipo de servicio es: " + serviciosCli[i]);
+                System.out.println ("El monto de la factura es de: " + totalFactura);
+            }
+        }
+
+    }
+}
