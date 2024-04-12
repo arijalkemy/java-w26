a669064e74d2b4b0dfc88d9823801ed4c581ec4a
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-04-12 15:06:24 -0300

    probar los tres casos

--- 1. Modulo Java/10. Ejercicio Integrador/src/main/java/meli/bootcamp/Main.java
+++ 1. Modulo Java/10. Ejercicio Integrador/src/main/java/meli/bootcamp/Main.java
@@ -21,17 +21,17 @@ public class Main {
     Reserva reservaTransporte = new Reserva(TipoReserva.TRANSPORTE, 300);
 
     // Se crearía un localizador con las reservas seleccionadas por el cliente que se guardaría en el repositorio
-    Localizador localizador = new Localizador(
+    Localizador localizadorPaqueteCompleto = new Localizador(
         List.of(reservaHotel, reservaComida, reservaViaje, reservaTransporte), cliente
     );
 
-    repositorioLocalizadores.agregarLocalizador(localizador);
+    repositorioLocalizadores.agregarLocalizador(localizadorPaqueteCompleto);
 
     // En algun momento se calcula el precio final del paquete y se llama a los siguientes metodos
     // Quien tenga esta responsabilidad debe conocer al repositorio de localizadores
     // y saber cual es el descuento por haber adquirido dos localizadores antes
-    double precioSinDescuentos = localizador.calcularTotalSinDescuento(); // para mostrar el total
-    double descuento = localizador.calcularTotalDeDescuentos(); // para obtener el descuento total
+    double precioSinDescuentos = localizadorPaqueteCompleto.calcularTotalSinDescuento(); // para mostrar el total
+    double descuento = localizadorPaqueteCompleto.calcularTotalDeDescuentos(); // para obtener el descuento total
 
     double porcentajeDeDescuentoMasDeDosLocalizadores = 0.1;
     if(repositorioLocalizadores.adquirioMasDeDosLocalizadores(cliente)) {
@@ -40,6 +40,38 @@ public class Main {
 
     System.out.println("Precio sin descuentos: " + precioSinDescuentos);
     System.out.println("Descuento de:          " + descuento);
-    System.out.println("Precio final:          " + (precioSinDescuentos - descuento));
+    System.out.println("Precio final:          " + (precioSinDescuentos - descuento) + "\n\n");
+
+    // Localizador con 2 reservas de hotel
+    Localizador localizadorConDosReservasDeHotel = new Localizador(List.of(reservaHotel, reservaHotel), cliente);
+    repositorioLocalizadores.agregarLocalizador(localizadorConDosReservasDeHotel);
+
+    // Lógica para calcular el precio final del paquete
+     precioSinDescuentos = localizadorConDosReservasDeHotel.calcularTotalSinDescuento(); // para mostrar el total
+     descuento = localizadorConDosReservasDeHotel.calcularTotalDeDescuentos(); // para obtener el descuento total
+
+    if(repositorioLocalizadores.adquirioMasDeDosLocalizadores(cliente)) {
+      descuento += precioSinDescuentos * porcentajeDeDescuentoMasDeDosLocalizadores;
+    }
+
+    System.out.println("Precio sin descuentos: " + precioSinDescuentos);
+    System.out.println("Descuento de:          " + descuento);
+    System.out.println("Precio final:          " + (precioSinDescuentos - descuento) + "\n\n");
+
+    // Localizador con descuento de 2 localizadores anteriores
+    Localizador localizadorConDescuentoPorTener2 = new Localizador(List.of(reservaHotel, reservaHotel), cliente);
+    repositorioLocalizadores.agregarLocalizador(localizadorConDescuentoPorTener2);
+
+    // Lógica para calcular el precio final del paquete
+    precioSinDescuentos = localizadorConDescuentoPorTener2.calcularTotalSinDescuento(); // para mostrar el total
+    descuento = localizadorConDescuentoPorTener2.calcularTotalDeDescuentos(); // para obtener el descuento total
+
+    if(repositorioLocalizadores.adquirioMasDeDosLocalizadores(cliente)) {
+      descuento += precioSinDescuentos * porcentajeDeDescuentoMasDeDosLocalizadores;
+    }
+
+    System.out.println("Precio sin descuentos: " + precioSinDescuentos);
+    System.out.println("Descuento de:          " + descuento);
+    System.out.println("Precio final:          " + (precioSinDescuentos - descuento) + "\n\n");
   }
 }
