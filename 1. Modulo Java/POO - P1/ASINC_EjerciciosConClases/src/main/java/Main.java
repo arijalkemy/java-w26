78948071d8fbddc0ba257ae3d3e0900d5d34ec59
marcosdittaML <marcos.ditta@mercadolibre.com>
2024-04-13 19:07:42 -0300

    Subo ejercicios asincronicos trabajando con clases

---
+++ 1. Modulo Java/POO - P1/ASINC_EjerciciosConClases/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Automovil auto = new Automovil("Volkswagen", "Blanco", 35650);
+        System.out.println(auto.mostrarMarcaYColor());
+    }
+}
