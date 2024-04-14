6f3769361c23cc8fb2a88539c9d5e8c613aea4d3
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-04-14 17:09:43 -0500

    added additional exercises

---
+++ 1. Modulo Java/0_Ejercicios_Adicionales/3_POO_P1_Automovil/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Automovil auto = new Automovil("Audi","Gris",1500);
+        System.out.println(auto.mostrarMarcaYColor());
+    }
+}
---
+++ 1. Modulo Java/0_Ejercicios_Adicionales/4_POO_P2_Excepciones/src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //Mensaje final
+        String mensajeFinal = "Este es el último mensaje";
+
+//Código que arroja excepción
+        try {
+            int[] numeros = new int[5];
+            numeros[5] = 10;
+        }catch(IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println(mensajeFinal);
+        }
+    }
+}
