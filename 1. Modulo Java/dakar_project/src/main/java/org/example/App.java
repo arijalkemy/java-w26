cf311a24fc4ba16966dea23c91d7679966ce4a40
Bryann Arlye Torres Rodríguez <164513573+BryannTorresMELI@users.noreply.github.com>
2024-04-13 21:22:15 -0500

    feat: create dakar project

---
+++ 1. Modulo Java/dakar_project/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // another practice https://drive.google.com/file/d/1Iwz5uN8q3_FdMAQ-BCkFxUC3nNM0hKon/view
+    }
+}
