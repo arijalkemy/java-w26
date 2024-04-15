73bda5875191bbc24f9de7d2116d56cba56af761
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-04-15 16:47:50 -0300

    agregar endpoint para traducir morse

---
+++ 1. Modulo Java/2. Codigo Morse/src/main/java/meli/bootcamp/codigo_morse/controllers/MorseController.java
@@ -0,0 +1,23 @@
+package meli.bootcamp.codigo_morse.controllers;
+
+import meli.bootcamp.codigo_morse.service.MorseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MorseController {
+  MorseService morseService;
+
+  @Autowired
+  public MorseController(MorseService morseService) {
+    this.morseService = morseService;
+  }
+
+  @GetMapping("/morse/{morseCode}")
+  public String translateToText(@PathVariable String morseCode) {
+    return MorseService.translateToText(morseCode);
+  }
+
+}
