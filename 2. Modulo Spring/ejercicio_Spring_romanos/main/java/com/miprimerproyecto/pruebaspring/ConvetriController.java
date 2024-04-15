7dece71eb616b9ad4eba5bf53ecf73831779e313
agustin.chiaravalli@mercadolibre.com <164513438+AaChiaravalli@users.noreply.github.com>
2024-04-15 12:13:36 -0300

    Subir ejercicio de nros romanos y utilización de Spring

---
+++ 2. Modulo Spring/ejercicio_Spring_romanos/main/java/com/miprimerproyecto/pruebaspring/ConvetriController.java
@@ -0,0 +1,31 @@
+package com.miprimerproyecto.pruebaspring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConvetriController {
+    @GetMapping("/convertir/{numero}")
+    public String convertirANumeroRomano(@PathVariable int numero) {
+        // Definir los símbolos romanos y sus valores decimales correspondientes
+        String[] simbolosRomanos = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int[] valoresDecimales = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+
+        StringBuilder resultado = new StringBuilder();
+
+        // Iterar a través de los símbolos romanos
+        int indice = valoresDecimales.length - 1; // Comenzar desde el símbolo de mayor valor
+        while (numero > 0) {
+            int divisor = numero / valoresDecimales[indice];
+            numero %= valoresDecimales[indice];
+            while (divisor > 0) {
+                resultado.append(simbolosRomanos[indice]);
+                divisor--;
+            }
+            indice--;
+        }
+
+        return resultado.toString();
+    }
+}
