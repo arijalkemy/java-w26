f583ce884deb2379bd3e37a7e6922a57ca0fef6f
Edwin Steven Guayacan <edwin.guayacan@mercadolibre.com.co>
2024-04-15 15:56:42 -0500

    Add codigo morse exercise

---
+++ 2. Modulo Spring/codigomorse/src/main/java/com/example/codigomorse/MorseRestController.java
@@ -0,0 +1,94 @@
+package com.example.codigomorse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MorseRestController {
+
+
+    private Map<String, Character> codigos = new HashMap<>();
+
+    public MorseRestController() {
+        codigos.put(".-", 'A');
+        codigos.put("-...", 'B');
+        codigos.put("-.-.", 'C');
+        codigos.put("-..", 'D');
+        codigos.put(".", 'E');
+        codigos.put("..-.", 'F');
+        codigos.put("--.", 'G');
+        codigos.put("....", 'H');
+        codigos.put("..", 'I');
+        codigos.put(".---", 'J');
+        codigos.put("-.-", 'K');
+        codigos.put(".-..", 'L');
+        codigos.put("--", 'M');
+        codigos.put("-.", 'N');
+        codigos.put("---", 'O');
+        codigos.put(".--.", 'P');
+        codigos.put("--.-", 'Q');
+        codigos.put(".-.", 'R');
+        codigos.put("...", 'S');
+        codigos.put("-", 'T');
+        codigos.put("..-", 'U');
+        codigos.put("...-", 'V');
+        codigos.put(".--", 'W');
+        codigos.put("-..-", 'X');
+        codigos.put("-.--", 'Y');
+        codigos.put("--..", 'Z');
+        codigos.put(".----", '1');
+        codigos.put("..---", '2');
+        codigos.put("...--", '3');
+        codigos.put("....-", '4');
+        codigos.put(".....", '5');
+        codigos.put("-....", '6');
+        codigos.put("--...", '7');
+        codigos.put("---..", '8');
+        codigos.put("----.", '9');
+        codigos.put("-----", '0');
+    }
+
+    @GetMapping("/{morse}")
+    public String traducirAEspañol(@PathVariable String morse) {
+        StringBuilder traduccion = new StringBuilder();
+        String[] palabras = morse.split("   ");
+
+        for (String palabra : palabras
+        ) {
+            String[] caracteres = palabra.split(" ");
+            for (String caracter : caracteres
+            ) {
+                if (codigos.containsKey(caracter)) traduccion.append(codigos.get(caracter));
+            }
+            traduccion.append(" ");
+
+        }
+
+        return traduccion.toString().trim();
+
+    }
+
+    @GetMapping("/2/{mensaje}")
+    public String traducirAMorse(@PathVariable String mensaje) {
+        StringBuilder traduccion = new StringBuilder();
+        for (char caracter : mensaje.toUpperCase().toCharArray()) {
+            if (caracter == ' ') {
+                traduccion.append("   ");
+            } else {
+                for (Map.Entry<String, Character> entry : codigos.entrySet()) {
+                    if (caracter == entry.getValue()) {
+                        traduccion.append(entry.getKey()).append(" ");
+                        break;
+                    }
+                }
+            }
+        }
+        return traduccion.toString().trim();
+    }
+
+
+}
