a172937a93c98c6382a37c79f8f9311fb4416c9b
levianfrancisco <levianlastra@gmail.com>
2024-04-15 00:18:35 -0300

    ejercicio_bubleSort

---
+++ 1. Modulo Java/ejercicio_bubleSort/ejercicioBubleSort/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.example.Ordenamiento.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int[] array = {5, 2, 9, 1, 5, 6};
+        burbuja(array);
+        imprimirArreglo(array);
+    }
+}
---
+++ 1. Modulo Java/ejercicio_bubleSort/ejercicioBubleSort/src/main/java/org/example/Ordenamiento.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Ordenamiento {
+    public static int[] burbuja(int[] array) {
+        int n = array.length;
+        boolean intercambio;
+
+        for (int paso = 0; paso < n - 1; paso++) {
+            intercambio = false;
+            for (int i = 0; i < n - paso - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    intercambio = true;
+                }
+            }
+            if (!intercambio) {
+                break;
+            }
+        }
+
+        return array;
+    }
+
+    public static void imprimirArreglo(int[] array) {
+        for (int elemento : array) {
+            System.out.print(elemento + " ");
+        }
+    }
+}
