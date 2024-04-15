40eb479ed6c051c361571c3db523b3695bc0de45
Rodrigo Arguello <rodrigoemiliano.arguello@mercadolibre.com>
2024-04-15 16:01:47 -0300

    Se desarrollo el ejercicio numerosRomanos de Introduccion a Spring

---
+++ 2. Modulo Spring/IntroSpring-NumerosRomanos/src/main/java/com/example/introspringnumerosromanos/IntroSpringNumerosRomanosApplication.java
@@ -0,0 +1,13 @@
+package com.example.introspringnumerosromanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringNumerosRomanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringNumerosRomanosApplication.class, args);
+	}
+
+}
