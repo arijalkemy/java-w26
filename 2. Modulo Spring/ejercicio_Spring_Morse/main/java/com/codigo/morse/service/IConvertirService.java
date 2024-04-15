e469ea360f89e3a4248490bab94ef6cef2182c6d
agustin.chiaravalli@mercadolibre.com <164513438+AaChiaravalli@users.noreply.github.com>
2024-04-15 19:06:09 -0300

    Subida del ejercicio Morse

---
+++ 2. Modulo Spring/ejercicio_Spring_Morse/main/java/com/codigo/morse/service/IConvertirService.java
@@ -0,0 +1,9 @@
+package com.codigo.morse.service;
+
+public interface IConvertirService {
+    String convertirDeMorseANormal(String cadena);
+    String convertirDeNormalAMorse(String cadena);
+
+}
+
+
