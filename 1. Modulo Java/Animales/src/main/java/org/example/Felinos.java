efa9f9ab004ec92ff5f1099fb3ea6a46322738b0
dbellolopez <166174567+dbellolopez@users.noreply.github.com>
2024-04-15 16:55:58 -0500

    Este no iba xd

--- 1. Modulo Java/Animales/src/main/java/org/example/Felinos.java
+++
@@ -1,9 +0,0 @@
-package org.example.;
-public abstract class Felinos extends Animal {
-    public String color;
-    public Felinos(String sonidos, String alimentos, String habitat, String nombreCientifico, String color) {
-        super(sonidos, alimentos, habitat, nombreCientifico);
-        this.color = color;
-    }
-
-}
