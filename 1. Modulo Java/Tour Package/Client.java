0aaac792e7e9e9e1f307a3b06c4c9a9fbc669052
Edwin Andres Villarraga Ardila <edwin.villarragaardila@mercadolibre.com.co>
2024-04-16 07:40:57 -0500

    FEAT: solucion de ejercicio de tour usando objetos y stream

---
+++ 1. Modulo Java/Tour Package/Client.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+public class Client {
+    private Integer id;
+    private String name;
+    private String lastname;
+    private boolean discount;
+
+    public Client(Integer id, String name, String lastname) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.discount = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public boolean getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(boolean discount) {
+        this.discount = discount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "[Client]" + " id: " + this.id + " name: " + name + " lastname: " + lastname + " discount: " + discount;
+    }
+}
---
+++ 1. Modulo Java/Tour Package/Invoice.java
@@ -0,0 +1,44 @@
+public class Invoice {
+    private TourPackage reservas;
+    private Client client;
+
+    public Invoice(TourPackage reserva, Client client) {
+        this.reservas = reserva;
+        this.client = client;
+    }
+
+    public TourPackage getReserva() {
+        return reservas;
+    }
+
+    public void setReservas(TourPackage reservas) {
+        this.reservas = reservas;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public double getPriceDiscount(){
+        double discount = 1.0;
+        if (reservas.getTicket() > 0 && reservas.getFood() > 0 && reservas.getHotel() > 0 && reservas.getTransport() > 0) {
+            discount -= 0.10;
+        }
+        if(reservas.getHotel() == 2 || reservas.getTicket() == 2){
+           discount -= 0.05;
+        }
+        if(client.getDiscount()){
+            discount -= 0.05;
+        }
+        return reservas.getPrice() * discount;
+    }
+
+    @Override
+    public String toString() {
+        return client + " - " + String.join("\n\t", this.reservas.toString());
+    }
+}
