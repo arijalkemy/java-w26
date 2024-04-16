d8122f41009752e12a85347155752a5c0c42f9fb
Brian Zarate <brian.zarate@mercadolibre.com>
2024-04-16 14:54:15 -0300

    2 Spring - agregado ejercicio calcular edad a travez de parametros de la url

---
+++ 2. Modulo Spring/arquitectura-rest-dto-response-entity/arqrestvivo/src/test/java/com/bootcamp/arqrestvivo/ArqrestvivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.arqrestvivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqrestvivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
