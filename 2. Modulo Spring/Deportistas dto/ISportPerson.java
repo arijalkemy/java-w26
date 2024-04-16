e7e9484e09f75478dc7060050a2f1e1defa9809c
Edwin Andres Villarraga Ardila <edwin.villarragaardila@mercadolibre.com.co>
2024-04-16 16:07:28 -0500

    FIX: organizacion del proyecto

--- 2. Modulo Spring/Deportistas dto/ISportPerson.java
+++
@@ -1,9 +0,0 @@
-package org.example.dtosport.service;
-
-import org.example.dtosport.entity.dto.SportPersonDto;
-
-import java.util.List;
-
-public interface ISportPerson {
-    List<SportPersonDto> findSportPerson();
-}
