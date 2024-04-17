b7e8fd81ed6f5404ef775357ef6c62f8170f3f35
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-04-17 15:04:27 -0300

    set up calculadora de calorias

---
+++ 2. Modulo Spring/7. Calculadora de Calorias/src/test/java/meli/bootcamp/star_wars/ApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.star_wars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
