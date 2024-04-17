5abbf1657034d6e19438a9dfe438a0a5efa8cd22
icugura_meli <ignacio.cugura@mercadolibre.com>
2024-04-17 17:07:43 -0300

    Se empieza el ejercicio de calorias

---
+++ 2. Modulo Spring/Ej_CalculadoraCalorias/src/main/java/bootcamp/spring/calculadora_calorias/controllers/CalculadoraCaloriasController.java
@@ -0,0 +1,9 @@
+package bootcamp.spring.calculadora_calorias.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculadoraCaloriasController {
+
+}
