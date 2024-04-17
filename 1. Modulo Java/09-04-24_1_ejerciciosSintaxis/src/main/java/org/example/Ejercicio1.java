764837ac07e9cbc24617fad05297e3a7e883303c
Leonardo Camargo <leonardocamargosapien@gmail.com>
2024-04-16 20:54:41 -0600

    primer ejercicio java

---
+++ 1. Modulo Java/09-04-24_1_ejerciciosSintaxis/src/main/java/org/example/Ejercicio1.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * Un estudiante de programación intentó realizar declaraciones de variables con sus respectivos tipos en Java pero
+ * tuvo varias dudas mientras lo hacía. A partir de esto, nos brindó su código y pidió la ayuda de un desarrollador
+ * experimentado que pueda darle una mano. ¿Podrías verificar su código y realizar las correcciones necesarias?
+ * Pistas:
+ * Recordá que en Java cada línea de código tiene una forma particular de cerrarse.
+ * Recordá que Java es un lenguaje FUERTEMENTE TIPADO, por lo que el correcto tipo de dato es muy importante en la
+ * declaración de cada variable
+ */
+
+public class Ejercicio1
+{
+    public static void main( String[] args )
+    {
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean estaVivo = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";    }
+}
---
+++ 1. Modulo Java/09-04-24_1_ejerciciosSintaxis/src/main/java/org/example/Ejercicio2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Un profesor de programación está corrigiendo los exámenes de sus estudiantes de la materia Programación I para
+ * poder brindarles las correspondientes devoluciones. Uno de los puntos del examen consistía en proponer nombres de
+ * variables, pero uno de los alumnos propuso nombres bastante peculiares. ¿Podrías ayudar al profesor a detectar
+ * cuáles de estos nombres que utilizó el alumno no son correctos?
+ * Pistas:
+ * Recordá que existen una serie de reglas a la hora de definir nombres de variables.
+ */
+
+public class Ejercicio2 {
+
+}
