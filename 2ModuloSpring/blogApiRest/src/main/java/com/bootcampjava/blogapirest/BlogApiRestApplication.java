fb43b5eb9d4f65278edd5089f9a07e43dff5f9a0
JmCoroMeli <juanmanuel.coro@mercadolibre.com>
2024-04-18 10:33:09 -0300

    creacion proyecto blog rest api

---
+++ 2ModuloSpring/blogApiRest/src/main/java/com/bootcampjava/blogapirest/BlogApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.bootcampjava.blogapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApiRestApplication.class, args);
+    }
+
+}
---
+++ 2ModuloSpring/blogApiRest/src/test/java/com/bootcampjava/blogapirest/BlogApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampjava.blogapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
