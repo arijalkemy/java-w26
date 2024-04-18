2c3c06553e91cb820fd096986f1b0bbbfc833010
miaramosml <mia.ramos@mercadolibre.com>
2024-04-17 22:17:21 -0300

    reordeno carpetas y subo el biolerplate para el ejercicio de concesionaria de autos

---
+++ 2. Modulo Spring/Ejercicios Prácticos - P2 - VIVO/Concesionaria-de-autos/src/test/java/org/example/concesionariadeautos/ConcesionariaDeAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.concesionariadeautos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionariaDeAutosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
