7a0c18d50dc3e89f5d78661d695a6875930e7e52
Bryann Arlye Torres Rodríguez <164513573+BryannTorresMELI@users.noreply.github.com>
2024-04-19 09:15:20 -0500

    feat: global project for code review

---
+++ 2. Modulo Spring/EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/service/VehicleServiceImpl.java
@@ -0,0 +1,33 @@
+package com.bootcampW22.EjercicioGlobal.service;
+
+import com.bootcampW22.EjercicioGlobal.dto.VehicleDto;
+import com.bootcampW22.EjercicioGlobal.entity.Vehicle;
+import com.bootcampW22.EjercicioGlobal.exception.NotFoundException;
+import com.bootcampW22.EjercicioGlobal.repository.IVehicleRepository;
+import com.bootcampW22.EjercicioGlobal.repository.VehicleRepositoryImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class VehicleServiceImpl implements IVehicleService{
+
+    IVehicleRepository vehicleRepository;
+
+    public VehicleServiceImpl(VehicleRepositoryImpl vehicleRepository){
+        this.vehicleRepository = vehicleRepository;
+    }
+    @Override
+    public List<VehicleDto> searchAllVehicles() {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Vehicle> vehicleList = vehicleRepository.findAll();
+        if(vehicleList.isEmpty()){
+            throw new NotFoundException("No se encontró ningun auto en el sistema.");
+        }
+        return vehicleList.stream()
+                .map(v -> mapper.convertValue(v,VehicleDto.class))
+                .collect(Collectors.toList());
+    }
+}
