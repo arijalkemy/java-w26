d85a9461171ffcc72294c511ac760df884a7c8d0
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-04-22 18:12:05 -0300

    fix: corregir parametos del endpoint crear entradas

--- 3. Sprint I/src/main/java/meli/bootcamp/star_wars/controller/BlogController.java
+++ 3. Sprint I/src/main/java/meli/bootcamp/star_wars/controller/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class BlogController {
   }
 
   @PostMapping
-  public ResponseEntity<EntradaBlogDto> crearEntradaBlog(EntradaBlogDto entradaBlog) {
+  public ResponseEntity<EntradaBlogDto> crearEntradaBlog(@RequestBody EntradaBlogDto entradaBlog) {
     return ResponseEntity.ok().body(blogService.crearEntradaBlog(entradaBlog));
   }
 }
