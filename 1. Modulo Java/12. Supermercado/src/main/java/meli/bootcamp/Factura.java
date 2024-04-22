d9c2b44aeb6d43992b702026ea51b47a36260703
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-04-22 15:05:30 -0300

    agregar factura

---
+++ 1. Modulo Java/12. Supermercado/src/main/java/meli/bootcamp/Factura.java
@@ -0,0 +1,30 @@
+package meli.bootcamp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Factura {
+  private Cliente cliente;
+  private final Map<Item, Integer> items; // <Item, cantidad>
+
+  public Factura(Cliente cliente) {
+    this.cliente = cliente;
+    this.items = new HashMap<>();
+  }
+
+  public void agregarItem(Item item, Integer unidades) {
+    if (items.containsKey(item)) {
+      items.put(item, items.get(item) + unidades);
+    } else {
+      items.put(item, unidades);
+    }
+  }
+
+  public Cliente getCliente() {
+    return cliente;
+  }
+
+  public Factura(Map<Item, Integer> items) {
+    this.items = items;
+  }
+}
