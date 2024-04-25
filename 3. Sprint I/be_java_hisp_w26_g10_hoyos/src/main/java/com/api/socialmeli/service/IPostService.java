41b28b832be2865e19c38618114cae23ff68ffd5
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-04-25 18:20:17 -0500

    Added the get products in discount by owner method to the post interface

--- 3. Sprint I/be_java_hisp_w26_g10_hoyos/src/main/java/com/api/socialmeli/service/IPostService.java
+++ 3. Sprint I/be_java_hisp_w26_g10_hoyos/src/main/java/com/api/socialmeli/service/IPostService.java
@@ -5,6 +5,8 @@ import com.api.socialmeli.dto.PostsByFollowedDto;
 import com.api.socialmeli.dto.PostDto;
 import com.api.socialmeli.dto.PromoPostDto;
 
+import java.util.List;
+
 public interface IPostService {
 
     PostsByFollowedDto getPostsByFollowed(Integer userId, String order);
@@ -14,4 +16,6 @@ public interface IPostService {
     PromoPostDto publishPromoPost(PromoPostDto promoPostDto);
 
     Integer CountProductsInPromoByOwner(Integer userId);
+
+    List<PromoPostDto> ProductsWithDiscountByOwner(Integer userId);
 }
