69e622d6644d9f178f035879f24206251e0d4f63
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-04-25 17:52:55 -0500

    Added general project solution

---
+++ 3. Sprint I/be_java_hisp_w26_g10_hoyos/src/main/java/com/api/socialmeli/service/ISellerService.java
@@ -0,0 +1,11 @@
+package com.api.socialmeli.service;
+
+import com.api.socialmeli.dto.FollowedBySellerDto;
+import com.api.socialmeli.dto.SellersCountFollowersDto;
+
+public interface ISellerService {
+
+    FollowedBySellerDto getFollowersOfSeller(int seller_id, String order);
+    SellersCountFollowersDto getCountOfSellerFollowers(Integer user_id);
+
+}
