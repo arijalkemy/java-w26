6a900c93757cf8a6c75e97c263026db963f03385
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-04-25 11:50:46 -0600

    UPDATE: Sprint 1 Individual

---
+++ 3. Sprint I/be_java_hisp_w26_g11_roman/src/main/java/org/example/sprint1/controller/SellerController.java
@@ -0,0 +1,61 @@
+package org.example.sprint1.controller;
+
+import jakarta.validation.Valid;
+import org.example.sprint1.dto.RequestPostDTO;
+import org.example.sprint1.dto.RequestPostPromoDTO;
+import org.example.sprint1.dto.ResponsePostDTO;
+import org.example.sprint1.entity.Seller;
+import org.example.sprint1.exception.BadRequestException;
+import org.example.sprint1.service.seller.ISellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class SellerController {
+
+    @Autowired
+    ISellerService sellerService;
+
+    @Validated
+    @PostMapping("/post")
+    public ResponseEntity<Void> addPost(@Valid @RequestBody RequestPostDTO postDTO, BindingResult result){
+        if(result.hasErrors()){
+            throw new BadRequestException("Bad Request");
+        }
+
+        sellerService.addPost(postDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @Validated
+    @PostMapping("/promo-post")
+    public ResponseEntity<RequestPostPromoDTO> addPostPromo(@Valid @RequestBody RequestPostPromoDTO postPromoDTO, BindingResult result){
+        if(result.hasErrors()){
+            throw new BadRequestException("Bad Request");
+        }
+        sellerService.addPost(postPromoDTO);
+        return ResponseEntity.ok().body(postPromoDTO);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Seller>> getAllSellers(){
+        return new ResponseEntity<>(sellerService.getSellers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity<ResponsePostDTO> getPostsFromFollowingWithTwoWeeksOld(
+            @PathVariable int userId,
+            @RequestParam Optional<String> order
+    ) {
+        return new ResponseEntity<>(sellerService.getPostsFromFollowingWithTwoWeeksOld(userId, order), HttpStatus.OK);
+    }
+
+}
---
+++ 3. Sprint I/be_java_hisp_w26_g11_roman/src/main/java/org/example/sprint1/dto/RequestPostPromoDTO.java
@@ -0,0 +1,23 @@
+package org.example.sprint1.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class RequestPostPromoDTO extends RequestPostDTO{
+
+    @JsonProperty("has_promo")
+    private boolean hasPromo;
+
+    private double discount;
+
+}
