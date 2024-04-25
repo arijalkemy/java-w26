4674d3c494e728e7c38e7e69e0cc5e116794d482
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-04-25 18:05:08 -0500

    Added the publish promo method to interface

--- 3. Sprint I/be_java_hisp_w26_g10_hoyos/src/main/java/com/api/socialmeli/service/IPostService.java
+++ 3. Sprint I/be_java_hisp_w26_g10_hoyos/src/main/java/com/api/socialmeli/service/IPostService.java
@@ -3,10 +3,13 @@ package com.api.socialmeli.service;
 import com.api.socialmeli.dto.PostsByFollowedDto;
 
 import com.api.socialmeli.dto.PostDto;
+import com.api.socialmeli.dto.PromoPostDto;
 
 public interface IPostService {
 
     PostsByFollowedDto getPostsByFollowed(Integer userId, String order);
 
     PostDto publishPost(PostDto postDto);
+
+    PromoPostDto publishPromoPost(PromoPostDto promoPostDto);
 }
