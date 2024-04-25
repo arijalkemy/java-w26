ee7ef7fc0e9ad870ca2ec192ecb7d36d10b04214
Brian Zarate <brian.zarate@mercadolibre.com>
2024-04-25 14:50:30 -0300

    Agregando Sprint 1 ejercicio grupal

---
+++ 3. Sprint I/be_java_hisp_w26_g14/src/main/java/org/example/g14/dto/ProductDto.java
@@ -0,0 +1,22 @@
+package org.example.g14.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonPropertyOrder({ "product_id", "product_name", "type", "brand", "color", "notes" })
+public class ProductDto {
+    @JsonProperty("product_id")
+    private int id;
+    @JsonProperty("product_name")
+    private String name;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+}
