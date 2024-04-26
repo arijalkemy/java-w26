bbc6bb06d3009b3b5acedf774f4d959af10ddb24
acontrerasch_meli <alexis.contreraschavez@mercadolibre.com.mx>
2024-04-26 08:36:50 -0600

    UPDATE: SellerFollowerDto

--- 3. Sprint I/sprint1/src/main/java/org/example/sprint1/dto/SellerFollowerDto.java
+++ 3. Sprint I/sprint1/src/main/java/org/example/sprint1/dto/SellerFollowerDto.java
@@ -1,6 +1,8 @@
 package org.example.sprint1.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,11 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonPropertyOrder({"user_id", "user_name", "followers"})
 public class SellerFollowerDto implements Serializable {
+    @JsonProperty("user_id")
     private int UserId;
+    @JsonProperty("user_name")
     private String sellerName;
     private List<BasicCustomerDto> followers;
 }
