3bce15105ac8b549d8e953e2ad58ff175396aaf8
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-04-26 01:53:40 -0500

    rebasing project

--- 3. Sprint I/be_java_hisp_w26_g10_hoyos/src/main/java/com/api/socialmeli/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.api.socialmeli.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
