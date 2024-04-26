03af4788c4ff05b2b39cfd3840c98388f6045e4d
mmalacaraML <manuel.malacara@mercadolibre.com.mx>
2024-04-25 23:39:08 -0600

    Sprint1 en equipo terminado

---
+++ 3. Sprint I/Sprint1Equipo/sprint1/src/main/java/com/example/sprint1/exception/ExceptionController.java
@@ -0,0 +1,33 @@
+package com.example.sprint1.exception;
+
+// removí import com.bootcampW22.EjercicioGlobal.dto.ExceptionDto;
+// agregué
+import com.example.sprint1.dto.ExceptionDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> notFound(NotFoundException e){
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AlreadyInUseException.class)
+    public ResponseEntity<?> alreadyInUse(AlreadyInUseException e){
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return new ResponseEntity<>(exceptionDto,HttpStatus.CONFLICT);
+    }
+
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> badRequest(BadRequestException e){
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return new ResponseEntity<>(exceptionDto,HttpStatus.BAD_REQUEST);
+    }
+}
