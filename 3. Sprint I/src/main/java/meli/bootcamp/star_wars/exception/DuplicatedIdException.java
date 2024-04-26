6d14ce888f908d554ee3d78ec8aba2327b01e048
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-04-26 15:35:53 -0300

    delete folder

--- 3. Sprint I/src/main/java/meli/bootcamp/star_wars/exception/DuplicatedIdException.java
+++
@@ -1,7 +0,0 @@
-package meli.bootcamp.star_wars.exception;
-
-public class DuplicatedIdException extends RuntimeException {
-  public DuplicatedIdException(String msg) {
-    super(msg);
-  }
-}
--- 3. Sprint I/src/main/java/meli/bootcamp/star_wars/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package meli.bootcamp.star_wars.exception;
-
-public class NotFoundException extends RuntimeException {
-  public NotFoundException(String msg) {
-    super(msg);
-  }
-}
