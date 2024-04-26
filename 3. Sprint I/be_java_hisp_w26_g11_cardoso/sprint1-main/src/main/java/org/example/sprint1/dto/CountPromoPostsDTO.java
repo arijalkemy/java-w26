e0c972a2a531b777821c07c1b503bde115efd62c
FranciscoCardosoRodriguez <franciscocardosorodriguez@gmail.com>
2024-04-26 00:05:10 -0600

    UPDATE: Borrados imports innecesarios del DTO de respuesta de US 0011

--- 3. Sprint I/be_java_hisp_w26_g11_cardoso/sprint1-main/src/main/java/org/example/sprint1/dto/CountPromoPostsDTO.java
+++ 3. Sprint I/be_java_hisp_w26_g11_cardoso/sprint1-main/src/main/java/org/example/sprint1/dto/CountPromoPostsDTO.java
@@ -1,17 +1,9 @@
 package org.example.sprint1.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.AssertTrue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDate;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
