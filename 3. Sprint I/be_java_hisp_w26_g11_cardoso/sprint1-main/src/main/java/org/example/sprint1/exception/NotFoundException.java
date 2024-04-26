9b5f6e3763c359983f658aaddd71ef4bf17c2022
FranciscoCardosoRodriguez <franciscocardosorodriguez@gmail.com>
2024-04-26 11:59:31 -0600

    ADD: Parte individual pushead a la rama del repo del sprint

--- 3. Sprint I/be_java_hisp_w26_g11_cardoso/sprint1-main/src/main/java/org/example/sprint1/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.example.sprint1.exception;
-
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
