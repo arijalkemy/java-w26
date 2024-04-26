e026b8bbcaa365bc384489b4a95d80cc01030a6b
Fabian Trujillo <fabian.trujillo@mercadolibre.com.co>
2024-04-25 19:36:41 -0500

    Carpeta con el sprint grupal

---
+++ 3. Sprint I/be_java_hisp_w26_g09_trujillo/src/main/java/com/meli/be_java_hisp_w26_g09/service/IPostService.java
@@ -0,0 +1,18 @@
+package com.meli.be_java_hisp_w26_g09.service;
+
+import com.meli.be_java_hisp_w26_g09.dto.PostDTO;
+import com.meli.be_java_hisp_w26_g09.dto.PromoProductsDTO;
+import com.meli.be_java_hisp_w26_g09.dto.ResponseDTO;
+import com.meli.be_java_hisp_w26_g09.dto.ProductFollowedListDTO;
+import com.meli.be_java_hisp_w26_g09.entity.Post;
+
+import java.util.List;
+
+public interface IPostService {
+    ResponseDTO addPost(PostDTO post);
+    ProductFollowedListDTO findFollowedPostsLastTwoWeeks(int userID);
+    ProductFollowedListDTO findFollowedPostsLastTwoWeeksSorted(int userID, String order);
+    ResponseDTO createPostWithPromo(PostDTO post);
+    PromoProductsDTO getPromoProducts(int userID);
+    List<Post> getAllPromosForId(int userID);
+}
---
+++ 3. Sprint I/be_java_hisp_w26_g09_trujillo/src/main/java/com/meli/be_java_hisp_w26_g09/util/mapper/ProductMapper.java
@@ -0,0 +1,20 @@
+package com.meli.be_java_hisp_w26_g09.util.mapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meli.be_java_hisp_w26_g09.dto.ProductDTO;
+import com.meli.be_java_hisp_w26_g09.entity.Product;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProductMapper {
+
+    public List<ProductDTO> productToProductDTO(List<Product> products) {
+        if (products == null)
+            return new ArrayList<>();
+
+        return products.stream().map(product -> new ProductDTO(product.getProductId(),product.getProductName(),product.getType(),product.getBrand(),product.getColor(),product.getNotes())).toList();
+    }
+}
