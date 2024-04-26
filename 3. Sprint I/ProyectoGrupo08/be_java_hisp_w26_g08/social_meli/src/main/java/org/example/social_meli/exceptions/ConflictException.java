50988ed731eb37047e32cefcd19b4468aac10576
abornacelly <aldo.bornacelly@mercadolibre.com.co>
2024-04-26 06:43:12 -0500

    Se agregan las carpetas del proyecto individual y grupal del sprint 1

---
+++ 3. Sprint I/ProyectoGrupo08/be_java_hisp_w26_g08/social_meli/src/main/java/org/example/social_meli/exceptions/ConflictException.java
@@ -0,0 +1,8 @@
+package org.example.social_meli.exceptions;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
---
+++ 3. Sprint I/ProyectoGrupo08/be_java_hisp_w26_g08/social_meli/src/main/java/org/example/social_meli/exceptions/ExceptionController.java
@@ -0,0 +1,29 @@
+package org.example.social_meli.exceptions;
+
+import org.example.social_meli.dto.ExceptionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handlerBadRequestException(BadRequestException ex){
+        ExceptionDTO exception = new ExceptionDTO(ex.getMessage());
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    private ResponseEntity<?> notFoundExceptionException(NotFoundException ex){
+        ExceptionDTO exception = new ExceptionDTO(ex.getMessage());
+        return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    private ResponseEntity<?> handlerConflictException(ConflictException ex){
+        ExceptionDTO exception = new ExceptionDTO(ex.getMessage());
+        return new ResponseEntity<>(exception, HttpStatus.CONFLICT);
+    }
+}
