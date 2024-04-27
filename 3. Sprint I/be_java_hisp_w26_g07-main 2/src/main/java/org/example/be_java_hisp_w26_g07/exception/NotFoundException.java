20ba2b94ad5b1eb12b4bd49929971292d9c134fd
ctorreschapa <cristianandres.torreschaparro@mercadolibre.com.co>
2024-04-27 10:48:05 -0500

    Actualizacion de archivos

--- 3. Sprint I/be_java_hisp_w26_g07-main 2/src/main/java/org/example/be_java_hisp_w26_g07/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.be_java_hisp_w26_g07.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
