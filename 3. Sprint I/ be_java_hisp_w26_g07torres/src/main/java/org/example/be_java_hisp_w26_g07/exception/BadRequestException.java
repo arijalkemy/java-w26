6b288123bdd643a1b42d6d734a644ae8c096ab28
ctorreschapa <cristianandres.torreschaparro@mercadolibre.com.co>
2024-04-27 10:26:46 -0500

    Modificación archivos local

--- 3. Sprint I/ be_java_hisp_w26_g07torres/src/main/java/org/example/be_java_hisp_w26_g07/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package org.example.be_java_hisp_w26_g07.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- 3. Sprint I/ be_java_hisp_w26_g07torres/src/main/java/org/example/be_java_hisp_w26_g07/exception/NotAcceptable.java
+++
@@ -1,8 +0,0 @@
-package org.example.be_java_hisp_w26_g07.exception;
-
-public class NotAcceptable extends RuntimeException {
-
-    public NotAcceptable(String message) {
-        super(message);
-    }
-}
