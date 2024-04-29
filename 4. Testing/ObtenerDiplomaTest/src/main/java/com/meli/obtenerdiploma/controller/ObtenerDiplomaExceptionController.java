3ca89bbd2c261a26280198107a87780a687e8680
gonzalopalomo <gonzalo.palomo@mercadolibre.com>
2024-04-29 16:44:18 -0300

    Se subre proyecto de test

---
+++ 4. Testing/ObtenerDiplomaTest/src/main/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaExceptionController.java
@@ -0,0 +1,45 @@
+package com.meli.obtenerdiploma.controller;
+
+import com.meli.obtenerdiploma.exception.ObtenerDiplomaException;
+import com.meli.obtenerdiploma.model.ErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class ObtenerDiplomaExceptionController {
+
+    @ExceptionHandler(ObtenerDiplomaException.class)
+    ResponseEntity<ErrorDTO> handleGlobalExceptions(ObtenerDiplomaException e) {
+        return new ResponseEntity<>(e.getError(), e.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException e) {
+        List<FieldError> errorFields = e.getFieldErrors();
+
+        Map<String, String> formattedErrors = new HashMap<>();
+        for (FieldError field : errorFields) {
+            formattedErrors.put(field.getField(), field.getDefaultMessage());
+        }
+        return new ResponseEntity<>(formattedErrors, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ErrorDTO> handleValidationExceptions(HttpMessageNotReadableException e) {
+        ErrorDTO error = new ErrorDTO("HttpMessageNotReadableException", e.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+
+}
