5aa1d98118466cad56abbaa67300d4dd6f99ef0e
Mauricio Jesus Pacheco <mauricio.pachecomijangos@mercadolibre.com.mx>
2024-04-29 13:39:53 -0600

    Update .gitignore to include test files

---
+++ 2. Modulo Spring/x. Concesionaria/src/test/java/org/bootcamp/concesionary/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bootcamp.concesionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
