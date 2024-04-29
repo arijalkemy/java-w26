9eff15fd1ff48767a67706f9f2a8a370a798f55c
Lautaro Oleastro <lautaro.oleastro@mercadolibre.com>
2024-04-29 14:19:11 -0300

    Agregadas las validaciones solicitadas en el enunciado.

--- 4. Testing/Introduccion a calidad de SW/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 4. Testing/Introduccion a calidad de SW/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -6,14 +6,19 @@ import lombok.Setter;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
-    @NotEmpty(message = "El nombre del estudiante no puede ser vacio")
+    @NotEmpty(message = "El nombre del alumno no puede ser vacío.")
+    @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
+    @Pattern(regexp = "^[A-Z].*", message = "El nombre del alumno tiene que comenzar con mayúscula.")
     String studentName;
     String message;
     Double averageScore;
     @Valid
+    @NotEmpty(message = "La lista no puede ser vacía.")
     List<SubjectDTO> subjects;
 }
--- 4. Testing/Introduccion a calidad de SW/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ 4. Testing/Introduccion a calidad de SW/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -3,17 +3,17 @@ package com.meli.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.*;
 
 @Getter @Setter
 public class SubjectDTO {
-    @NotEmpty(message = "El nombre de la materia no puede ser vacio")
+    @NotEmpty(message = "El nombre de la materia no puede estar vacío.")
+    @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.")
+    @Pattern(regexp = "^[A-Z].*", message = "El nombre de la materia tiene que comenzar con mayúscula.")
     String name;
 
-    @PositiveOrZero(message = "La nota no puede ser negativa")
-    @Max(value = 10, message = "La nota excede el limite (maximo 10)")
+    @NotNull(message = "La nota no puede estar vacía.")
+    @Min(value = 0, message = "La mínima nota es 0.0")
+    @Max(value = 10, message = "La máxima nota es 10.0")
     Double score;
 }
