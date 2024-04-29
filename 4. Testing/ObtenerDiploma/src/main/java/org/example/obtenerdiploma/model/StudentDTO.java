09e8fddd0def3c43d2dfb3cb70d6df333739a5b0
joel rodriguez <joel.rodriguez@mercadolibre.com>
2024-04-29 14:19:43 -0300

    change validations SubjectDTO

--- 4. Testing/ObtenerDiploma/src/main/java/org/example/obtenerdiploma/model/StudentDTO.java
+++ 4. Testing/ObtenerDiploma/src/main/java/org/example/obtenerdiploma/model/StudentDTO.java
@@ -14,10 +14,14 @@ public class StudentDTO {
     @Max(value = 50, message = "El nombre del estudiante no puede tener más de 50 caracteres")
     @Pattern(regexp = "^[A-Z].*", message = "El nombre del estudiante debe comenzar con una letra mayúscula")
     String studentName;
+
     @NotNull(message = "El mensaje no puede ser nulo")
     @NotEmpty(message = "El mensaje no puede ser vacío")
     String message;
+
+    @NotNull(message = "El promedio no puede ser nulo")
     Double averageScore;
+
     @NotNull(message = "La lista de materias no puede ser nula")
     @NotEmpty(message = "La lista de materias no puede ser vacía")
     List<SubjectDTO> subjects;
--- 4. Testing/ObtenerDiploma/src/main/java/org/example/obtenerdiploma/model/SubjectDTO.java
+++ 4. Testing/ObtenerDiploma/src/main/java/org/example/obtenerdiploma/model/SubjectDTO.java
@@ -1,7 +1,6 @@
 package org.example.obtenerdiploma.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +9,12 @@ import lombok.Setter;
 public class SubjectDTO {
     @NotNull(message = "El nombre de la materia no puede ser nulo")
     @NotBlank(message = "El nombre de la materia no puede ser vacío")
+    @Max(value = 30, message = "El nombre del estudiante no puede tener más de 30 caracteres")
+    @Pattern(regexp = "^[A-Z].*", message = "El nombre del estudiante debe comenzar con una letra mayúscula")
     String name;
 
     @NotNull(message = "La nota de la materia no puede ser nula")
+    @Max(value = 10, message = "La nota de la materia no puede ser mayor a 10")
+    @Min(value = 0, message = "La nota de la materia no puede ser menor a 0")
     Double score;
 }
