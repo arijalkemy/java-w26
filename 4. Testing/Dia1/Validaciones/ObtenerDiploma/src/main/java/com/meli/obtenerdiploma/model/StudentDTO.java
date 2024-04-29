e89b5aeca70f70511c917ac5f72982ba5f0d113e
ignacio-bertola <ignacio.bertola@mercadolibre.com>
2024-04-29 17:28:15 -0300

    subida resolucion ej validaciones y scaffold unit test

---
+++ 4. Testing/Dia1/Validaciones/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -0,0 +1,25 @@
+package com.meli.obtenerdiploma.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.*;
+import java.util.List;
+
+@Getter @Setter
+public class StudentDTO {
+
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
+    @Pattern(regexp = "^[A-Z].*", message = "El nombre del alumno debe empezar con mayúscula.")
+    @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
+    String studentName;
+
+    String message;
+
+    Double averageScore;
+
+    @NotNull(message = "La lista no puede estar vacía.")
+    @NotEmpty(message = "La lista no puede estar vacía.")
+    List<SubjectDTO> subjects;
+}
