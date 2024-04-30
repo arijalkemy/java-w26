727b680bbd37fb91645146bc5e07bfbeb3d10eb8
Lautaro Oleastro <lautaro.oleastro@mercadolibre.com>
2024-04-30 11:02:38 -0300

    Ejercicio 2 Validaciones y Test unitarios terminado

---
+++ 4. Testing/Validaciones + Tests Unitarios/DiplomaTestCrud/src/test/java/com/meli/obtenerdiploma/service/ObtenerDiplomaServiceTest.java
@@ -0,0 +1,81 @@
+package com.meli.obtenerdiploma.service;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import com.meli.obtenerdiploma.repository.IStudentDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ObtenerDiplomaServiceTest {
+    @Mock
+    private IStudentDAO studentDAO;
+    @InjectMocks
+    private ObtenerDiplomaService obtenerDiplomaService;
+    private StudentDTO studentWithHonorsDTO;
+    private StudentDTO studentWithoutHonorsDTO;
+
+    @BeforeEach
+    public void setup() {
+        this.studentWithoutHonorsDTO = new StudentDTO(
+                1L,
+                "EstudianteRegular",
+                "",
+                0.0,
+                List.of(
+                        new SubjectDTO("Matematica", 8.0),
+                        new SubjectDTO("Lengua", 8.0)
+                ));
+        this.studentWithHonorsDTO = new StudentDTO(
+                2L,
+                "EstudianteConHonores",
+                "",
+                0.0,
+                List.of(
+                        new SubjectDTO("Matematica", 10.0),
+                        new SubjectDTO("Lengua", 9.0)
+                ));
+
+    }
+
+    @Test
+    void analyzeScores() {
+        when(studentDAO.findById(2L)).thenReturn(studentWithHonorsDTO);
+        StudentDTO computedStudent = obtenerDiplomaService.analyzeScores(2L);
+        verify(studentDAO, atLeast(1)).findById(2L);
+        assertEquals(9.5, computedStudent.getAverageScore());
+    }
+
+    @Test
+    void analyzeMessageWithoutHonors() {
+        when(studentDAO.findById(1L)).thenReturn(studentWithoutHonorsDTO);
+        StudentDTO computedStudent = obtenerDiplomaService.analyzeScores(1L);
+        verify(studentDAO, atLeast(1)).findById(1L);
+        assertEquals(
+                "El alumno EstudianteRegular ha obtenido un promedio de 8. Puedes mejorar.",
+                computedStudent.getMessage()
+        );
+    }
+
+    @Test
+    void analyzeMessageWithHonors() {
+        when(studentDAO.findById(2L)).thenReturn(studentWithHonorsDTO);
+        StudentDTO computedStudent = obtenerDiplomaService.analyzeScores(2L);
+        verify(studentDAO, atLeast(1)).findById(2L);
+        assertEquals(
+                "El alumno EstudianteConHonores ha obtenido un promedio de 9,5. Felicitaciones!",
+                computedStudent.getMessage()
+        );
+    }
+}
