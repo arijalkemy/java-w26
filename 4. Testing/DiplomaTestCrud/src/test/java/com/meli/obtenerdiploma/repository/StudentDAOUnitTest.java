4015a4c137016803b625eac5f45c39d1bd321ee2
joel rodriguez <joel.rodriguez@mercadolibre.com>
2024-04-30 11:29:10 -0300

    add afterEach

--- 4. Testing/DiplomaTestCrud/src/test/java/com/meli/obtenerdiploma/repository/StudentDAOUnitTest.java
+++ 4. Testing/DiplomaTestCrud/src/test/java/com/meli/obtenerdiploma/repository/StudentDAOUnitTest.java
@@ -2,6 +2,7 @@ package com.meli.obtenerdiploma.repository;
 
 import com.meli.obtenerdiploma.model.StudentDTO;
 import com.meli.obtenerdiploma.model.SubjectDTO;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.mockito.Mockito.when;
 class StudentDAOUnitTest {
     SubjectDTO subjectDTO;
     StudentDTO studentDTO;
@@ -21,20 +22,30 @@ class StudentDAOUnitTest {
     void setUp() {
         studentDAO = new StudentDAO();
         subjectDTO = new SubjectDTO("Matematica", 10.0);
-        studentDTO = new StudentDTO(1L, "Juan", "Perez", 6.0, new ArrayList<>(List.of(subjectDTO)));
+        studentDTO = new StudentDTO(99L,
+                "Juan",
+                "Perez",
+                6.0,
+                new ArrayList<>(List.of(subjectDTO)));
+    }
+
+    @AfterEach
+    void tearDown() {
+        studentDAO.delete(99L);
+
+
+        studentDTO.setId(1L);
+        studentDAO.save(studentDTO);
     }
 
 
     @Test
     @DisplayName("Test save with non-existing or existing id")
     void save_nonExistingId() {
-        studentDTO.setId(999L);
+        studentDTO.setId(99L);
 
         long id = studentDAO.save(studentDTO);
         assertEquals(studentDTO.getId(), id);
-
-        long idDuplicated = studentDAO.save(studentDTO);
-        assertEquals(studentDTO.getId(), idDuplicated);
     }
 
     @Test
@@ -48,16 +59,8 @@ class StudentDAOUnitTest {
     @Test
     @DisplayName("Test delete with non-existing id")
     void delete_nonExistingId() {
-        studentDAO.save(studentDTO);
         boolean result = studentDAO.delete(99L);
         assertEquals(false, result);
     }
 
-
-    //buscar un alumno por id
-    @Test
-    void findById_existingId() {
-        Long id = 1L;
-        assertDoesNotThrow(() -> studentDAO.findById(id));
-    }
 }
