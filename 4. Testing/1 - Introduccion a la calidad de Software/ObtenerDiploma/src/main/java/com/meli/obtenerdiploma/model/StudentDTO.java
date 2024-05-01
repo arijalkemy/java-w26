766b65ae58453ef421f048e20c2a9381146d213b
juagonzalezf_meli <juanangel.jgonzalezflores@mercadolibre.com.mx>
2024-05-01 15:07:08 -0600

    refactor(StudentDTO): Validation annotation has been refactored according to the US-0001

--- 4. Testing/1 - Introduccion a la calidad de Software/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 4. Testing/1 - Introduccion a la calidad de Software/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -4,29 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
-    @NotNull(message = "el campo de studentName es requerido")
-    @NotEmpty(message = "el campo de studentName no puede estar vacio")
-    @Size(message = "el campo de studentName debe tener como minimo 5 caracteres")
+
+    @NotEmpty(message = "El nombre del alumno no puede estar vacio")
+    @Pattern(regexp = "[A-Z].*", message = "El nombre del alumno debe comenzar con mayuscula")
+    @Size(max=50, message = "La longitud del nombre no puede superar los 50 caracteres")
     String studentName;
 
-    @NotNull(message = "el campo de message es requerido")
-    @NotEmpty(message = "el campo de message no puede estar vacio")
     String message;
-
-
-    @NotNull(message = "el campo de averageScore es requerido")
-    @PositiveOrZero(message = "el campo de averageScore debe ser positivo o cero")
     Double averageScore;
-
-    @Valid
-    @Size(min = 1, message = "la lista de subjects debe de contener al menos un subject")
+    @Size(min = 1, message = "La lista no puede ser vacia")
     List<SubjectDTO> subjects;
 }
