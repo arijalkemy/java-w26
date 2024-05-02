c69f206fc46beedd607bc328ade18a49af2b5274
Mauricio Jesus Pacheco <mauricio.pachecomijangos@mercadolibre.com.mx>
2024-05-02 00:17:57 -0600

    Removed unused imports

--- 2. Modulo Spring/9. Blog_Manejo_Excepciones/src/main/java/org/bootcamp/blog/exception/ExceptionController.java
+++ 2. Modulo Spring/9. Blog_Manejo_Excepciones/src/main/java/org/bootcamp/blog/exception/ExceptionController.java
@@ -2,9 +2,7 @@ package org.bootcamp.blog.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
