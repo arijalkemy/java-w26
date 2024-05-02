5ace33f34daee7c19fc58a8d6101203e03c088bf
dbellol <dbellol@unal.edu.co>
2024-05-01 22:04:50 -0500

    Modificacion validaciones

--- 4. Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 4. Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -1,26 +1,24 @@
 package com.meli.obtenerdiploma.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.validation.constraints.Size;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
 public class StudentDTO {
-    @NotBlank(message = "El nombre del alumno no puede estar vacio")
+    @NotNull(message = "El nombre del alumno no puede estar vacio")
     @Pattern(regexp="^[A-Z].*", message ="El nombre del alumno debe iniciar con mayuscula" )
-    @Size(max=50, message = "La longitud máxima del nombre no puede superar los 50 caracteres")
+    @Max(value=50, message = "La longitud máxima del nombre no puede superar los 50 caracteres")
     String studentName;
     String message;
     Double averageScore;
-    @Size(min=1, message = "La lista no puede ser vacía")
+    @Min(value=1, message = "La lista no puede ser vacía")
     List<SubjectDTO> subjects;
 }
--- 4. Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ 4. Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -4,20 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.validation.constraints.Size;
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+
+import javax.validation.constraints.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
 public class SubjectDTO {
-    @NotBlank(message = "El nombre de la materia no puede estar vacío")
+    @NotNull(message = "El nombre de la materia no puede estar vacío")
     @Pattern(regexp="^[A-Z].*", message ="El nombre del alumno debe iniciar con mayuscula" )
-    @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.") // Nota que he cambiado el valor a 30 según tu mensaje de error.
+    @Max(value = 30, message = "La longitud del nombre no puede superar los 30 caracteres.") // Nota que he cambiado el valor a 30 según tu mensaje de error.
     String name;
-    @NotBlank(message = "La nota no puede estar vacía")
+    @NotNull(message = "La nota no puede estar vacía")
     @DecimalMax(value="10.0", message = "La nota máxima es de 10.0")
     @DecimalMin(value="0.0", message = "La nota mínima es de 0.0")
     Double score;
