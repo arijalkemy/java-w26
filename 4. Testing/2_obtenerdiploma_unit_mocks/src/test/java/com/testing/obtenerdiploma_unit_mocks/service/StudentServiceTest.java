504fd70b76c9b0551b0403bbe0b293ba51fdaef6
Matias Pinto <matiasdaniel.pinto@mercadolibre.com>
2024-05-03 11:53:08 -0300

    feat: ex 3 on StudentService

--- 4. Testing/2_obtenerdiploma_unit_mocks/src/test/java/com/testing/obtenerdiploma_unit_mocks/service/StudentServiceTest.java
+++ 4. Testing/2_obtenerdiploma_unit_mocks/src/test/java/com/testing/obtenerdiploma_unit_mocks/service/StudentServiceTest.java
@@ -1,15 +1,19 @@
 package com.testing.obtenerdiploma_unit_mocks.service;
 
 import com.testing.obtenerdiploma_unit_mocks.model.StudentDTO;
+import com.testing.obtenerdiploma_unit_mocks.repository.IStudentDAO;
 import com.testing.obtenerdiploma_unit_mocks.repository.IStudentRepository;
-import com.testing.obtenerdiploma_unit_mocks.repository.StudentRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -18,36 +22,66 @@ class StudentServiceTest {
     @Mock
     IStudentRepository studentRepository;
 
+    @Mock
+    IStudentDAO studentDAO;
+
     @InjectMocks
     StudentService studentService;
 
     @Test
     void create() {
+        // arrange
+        StudentDTO studentDTO = new StudentDTO(
+                0L,
+                "Pepito",
+                null,
+                null,
+                List.of()
+        );
 
-//        StudentDTO studentDTO = new StudentDTO(
-//                0L,
-//                "",
-//                "",
-//                0.0,
-//                new ArrayList<>()
-//        );
-//        this.studentService.create(studentDTO);
+        // act & assert
+        this.studentService.create(studentDTO);
+        verify(studentDAO, atLeastOnce()).save(studentDTO);
     }
 
     @Test
-    void read() {
+    void readKnownStudent() {
+        // Arrange
+        Long id = 1L;
+
+        StudentDTO studentDTO = new StudentDTO(
+                1L, "Pepito", null, null, List.of()
+        );
+
+        when(studentDAO.findById(id)).thenReturn(studentDTO);
+
+        // Act
+        StudentDTO response = this.studentService.read(id);
+
+        // Assert
+        Assertions.assertEquals(id, response.getId());
     }
 
     @Test
-    void update() {
+    void deleteNotAValidStudent() {
+        Long id = 100L;
+        this.studentService.delete(id);
+        verify(studentDAO).delete(id);
     }
 
     @Test
-    void delete() {
-    }
+    void getAllStudents() {
+        // Arrange
+        StudentDTO studentDTO = new StudentDTO(
+                1L, "Pepito", null, null, List.of()
+        );
 
-    @Test
-    void getAll() {
+        when(studentRepository.findAll()).thenReturn(Set.of(studentDTO));
 
+        // Act
+        Set<StudentDTO> results = this.studentService.getAll();
+
+        // Assert
+        Assertions.assertEquals(1, results.size());
     }
 }
