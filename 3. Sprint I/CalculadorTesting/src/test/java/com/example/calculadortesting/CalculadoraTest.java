2605dca00187e9f6223be79a21e6c586ae5e5dd7
AaChiaravalli <agustin.chiaravalli@mercadolibre.com>
2024-05-03 10:40:00 -0300

    Add first Exercise

---
+++ 3. Sprint I/CalculadorTesting/src/test/java/com/example/calculadortesting/CalculadoraTest.java
@@ -0,0 +1,2 @@
+package com.example.calculadortesting;public class CalculadoraTest {
+}
---
+++ 4. Testing/Introducción a la calidad de SW - P1 - Ejercicio Práctico/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.meli.obtenerdiploma;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.service.ObtenerDiplomaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObetenerDiplomaApplicationTests {
+}
