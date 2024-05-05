ef24332baba84de2c602cd32b2f8b5b7b44e0214
juagonzalezf_meli <juanangel.jgonzalezflores@mercadolibre.com.mx>
2024-05-05 15:05:59 -0600

    test(StudentDTO): methods equals and hashCode overrided in the StudentDTO

--- 4. Testing/2 - Validaciones + Test unitarios - VIVO/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 4. Testing/2 - Validaciones + Test unitarios - VIVO/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -1,14 +1,12 @@
 package com.meli.obtenerdiploma.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.util.List;
+import java.util.Objects;
 
 @Getter @Setter
 @AllArgsConstructor
@@ -27,4 +25,17 @@ public class StudentDTO {
 
     @NotEmpty(message = "La lista de materias no puede estar vacía.")
     List<@Valid SubjectDTO> subjects;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentDTO that = (StudentDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(studentName, that.studentName) && Objects.equals(message, that.message) && Objects.equals(averageScore, that.averageScore) && Objects.equals(subjects, that.subjects);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, studentName, message, averageScore, subjects);
+    }
 }
--- 4. Testing/2 - Validaciones + Test unitarios - VIVO/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/service/happyPath/OptenerDiplomaServiceHappyPathTest.java
+++ 4. Testing/2 - Validaciones + Test unitarios - VIVO/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/service/happyPath/OptenerDiplomaServiceHappyPathTest.java
@@ -1,28 +1,123 @@
 package com.meli.obtenerdiploma.service.happyPath;
 
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
 import com.meli.obtenerdiploma.repository.IStudentDAO;
 import com.meli.obtenerdiploma.repository.IStudentRepository;
+import com.meli.obtenerdiploma.repository.StudentDAO;
 import com.meli.obtenerdiploma.service.IObtenerDiplomaService;
+import com.meli.obtenerdiploma.service.ObtenerDiplomaService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class OptenerDiplomaServiceHappyPathTest {
 
     @Mock
-    IStudentDAO studentDAO;
-
-    @Mock
-    IStudentRepository studentRepository;
+    StudentDAO studentDAO;
 
     @InjectMocks
-    IObtenerDiplomaService obtenerDiplomaService;
+    ObtenerDiplomaService obtenerDiplomaService;
 
     @BeforeEach
     public void setup(){
-        
+        StudentDTO student1 = new StudentDTO(
+                0L,
+                "Jose",
+                "",
+                0.0,
+                Arrays.asList(
+                        new SubjectDTO(
+                                "Fisica",
+                                10.0
+                        ),
+                        new SubjectDTO(
+                                "Graficacion",
+                                9.0
+                        ),
+                        new SubjectDTO(
+                                "Circuitos",
+                                6.0
+                        )
+                )
+        );
+
+        StudentDTO student2 = new StudentDTO(
+                0L,
+                "Abel",
+                "",
+                0.0,
+                Arrays.asList(
+                        new SubjectDTO(
+                                "Matematicas",
+                                8.0
+                        ),
+                        new SubjectDTO(
+                                "Calculo",
+                                9.0
+                        ),
+                        new SubjectDTO(
+                                "IA",
+                                6.0
+                        )
+                )
+        );
+
+        studentDAO.save(student1);
+        studentDAO.save(student2);
+    }
+
+    @Test
+    @DisplayName("it should return the same StudentDTO in input/output")
+    public void studentDTOInputAndOutput(){
+        // arrange
+        StudentDTO studentInput = new StudentDTO(
+                1L,
+                "Jose",
+                "",
+                0.0,
+                new ArrayList<SubjectDTO>(Arrays.asList(
+                        new SubjectDTO(
+                                "Fisica",
+                                10.0
+                        ),
+                        new SubjectDTO(
+                                "Graficacion",
+                                9.0
+                        ),
+                        new SubjectDTO(
+                                "Circuitos",
+                                6.0
+                        )
+                )) {}
+        );
+
+        // act
+        when(studentDAO.findById(1L)).thenReturn(studentInput);
+        StudentDTO studentOutput  = obtenerDiplomaService.analyzeScores(studentInput.getId());
+
+        // assert
+        assertEquals(studentInput, studentOutput);
+    }
+
+    @Test
+    @DisplayName("it should return a correct AnalyzeScore of the user with Id 1")
+    public void analyzeScoreTest(){
+
     }
 }
