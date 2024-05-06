c8b74cc2f4e589223cbf8dc690acb9cce2ab02cd
JmCoroMeli <juanmanuel.coro@mercadolibre.com>
2024-05-06 00:20:07 -0300

    Primer test unitario :)

--- 4. Testing/ObtenerDiplomaUnitTest/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
+++ 4. Testing/ObtenerDiplomaUnitTest/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
@@ -1,13 +1,67 @@
 package com.meli.obtenerdiploma;
 
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import com.meli.obtenerdiploma.repository.StudentDAO;
+import com.meli.obtenerdiploma.service.ObtenerDiplomaService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ObetenerDiplomaApplicationTests {
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
-	@Test
-	public void contextLoads() {
+import static org.mockito.Mockito.*;
+
+
+@RequiredArgsConstructor
+@ExtendWith(MockitoExtension.class)
+class ObetenerDiplomaApplicationTests {
+	@Mock
+	StudentDAO studentDao = new StudentDAO();
+	@InjectMocks
+	private ObtenerDiplomaService diplomaService;
+	private StudentDTO student;
+
+	@BeforeEach
+	private void setup(){
+		// seteamos el usuario que probaremos
+		List<SubjectDTO> subjects = new ArrayList<>();
+		SubjectDTO subjectOne = new SubjectDTO("matematica",9.0);
+		SubjectDTO subjectTwo = new SubjectDTO("lengua", 0.0);
+		SubjectDTO subjectThree = new SubjectDTO("fisica", 10.0);
+		subjects.add(subjectOne);
+		subjects.add(subjectTwo);
+		subjects.add(subjectThree);
+		student = new StudentDTO();
+		student.setId(1L);
+		student.setStudentName("Juan Perez");
+		student.setSubjects(subjects);
 
 	}
+
+	@Test
+	@DisplayName("Juan tiene promedio 6.33")
+	void TestObtenerDiplomaService(){
+
+		//arrange
+		when(studentDao.findById(1L)).thenReturn(student);
+		//act
+		StudentDTO result = diplomaService.analyzeScores(1L);
+		//asserts
+
+		Assertions.assertEquals(result.getAverageScore(), (double) 19/3);
+
+	}
+
 }
---
+++ 4. Testing/ObtenerDiplomaUnitTest/src/test/java/com/meli/obtenerdiploma/StudentRepositoryTests.java
@@ -0,0 +1,11 @@
+package com.meli.obtenerdiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StudentRepositoryTests {
+
+
+
+}
