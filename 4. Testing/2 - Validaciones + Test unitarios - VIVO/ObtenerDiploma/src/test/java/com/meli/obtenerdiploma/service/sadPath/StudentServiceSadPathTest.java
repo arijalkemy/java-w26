87882ba87188a73c10aff1e20db79fb7e1e519f3
juagonzalezf_meli <juanangel.jgonzalezflores@mercadolibre.com.mx>
2024-05-05 19:01:31 -0600

    test(StudentServiceSadPathTest): initial class test added

---
+++ 4. Testing/2 - Validaciones + Test unitarios - VIVO/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/service/sadPath/StudentServiceSadPathTest.java
@@ -0,0 +1,21 @@
+package com.meli.obtenerdiploma.service.sadPath;
+
+import com.meli.obtenerdiploma.repository.IStudentDAO;
+import com.meli.obtenerdiploma.repository.IStudentRepository;
+import com.meli.obtenerdiploma.service.StudentService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class StudentServiceSadPathTest {
+    @Mock
+    IStudentDAO studentDAO;
+
+    @Mock
+    IStudentRepository studentRepository;
+
+    @InjectMocks
+    StudentService studentService;
+}
