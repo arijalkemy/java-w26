c7f353f583b71528f9517ec047d26deb0fb29729
blbulacio_meli <blas.bbulacio@mercadolibre.com>
2024-05-06 11:18:22 -0300

    calculadora integration tests

--- 4. Testing/5-Calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/controller/integration/CalculateRestControllerIntegrationTest.java
+++ 4. Testing/5-Calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/controller/integration/CalculateRestControllerIntegrationTest.java
@@ -1,35 +1,62 @@
 package com.mercadolibre.calculadorametroscuadrados.controller.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.mercadolibre.calculadorametroscuadrados.dto.HouseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseResponseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.RoomDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import utils.TestUtils;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
-public class CalculateRestControllerIntegrationTest {
+class CalculateRestControllerIntegrationTest {
+    @Autowired
     MockMvc mockMvc;
 
-    ObjectWriter writer = new ObjectWriter().
+    ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer();
 
     @Test ()
-    @DisplayName("Should calculate price successfully when there are no rooms")
-    void shouldCalculatePriceSuccessfully() {
+    @DisplayName("Should handle empty rooms correctly")
+    void shouldHandleEmptyRoomsCorrectly() throws Exception {
         HouseDTO house = TestUtils.createHouseDTO(new ArrayList<>());
+        HouseResponseDTO expectedHouse = new HouseResponseDTO(house);
+        expectedHouse.setSquareFeet(0);
+        expectedHouse.setPrice(0);
+
+        String payloadJson = writer.writeValueAsString(house);
+        String expectedResponseJson = writer.writeValueAsString(expectedHouse);
+
+        MvcResult mvcResult = mockMvc.perform(post("/calculate")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(payloadJson))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        String actualResponseJson = mvcResult.getResponse().getContentAsString();
 
 
-//        MvcResult mvcResult = mockMvc.perform(get("/calculate"))
+        Assertions.assertEquals(expectedResponseJson, actualResponseJson);
     }
 }
