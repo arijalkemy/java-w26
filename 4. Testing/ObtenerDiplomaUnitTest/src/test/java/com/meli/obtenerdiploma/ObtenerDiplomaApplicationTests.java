2740ad53fa104a323d99978924025054676f11f4
JmCoroMeli <juanmanuel.coro@mercadolibre.com>
2024-05-06 00:34:22 -0300

    add new units test :)

--- 4. Testing/ObtenerDiplomaUnitTest/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
+++ 4. Testing/ObtenerDiplomaUnitTest/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
@@ -28,40 +28,61 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class ObetenerDiplomaApplicationTests {
 	@Mock
-	StudentDAO studentDao = new StudentDAO();
+	private static StudentDAO studentDao = new StudentDAO();
 	@InjectMocks
 	private ObtenerDiplomaService diplomaService;
 	private StudentDTO student;
-
+	List<SubjectDTO> subjects = new ArrayList<>();
 	@BeforeEach
 	private void setup(){
-		// seteamos el usuario que probaremos
-		List<SubjectDTO> subjects = new ArrayList<>();
+		// seteamos el usuario que probaremos, dejamos la lista de
+		// //materias vacias asi dependiendo la prueba podremos probar los casos borde
+		student = new StudentDTO();
+		student.setId(1L);
+		student.setStudentName("Juan Perez");
+	}
+
+	@Test
+	@DisplayName("Juan tiene promedio 6.33")
+	void TestObtenerDiplomaDebeMejorar(){
+		String expectedMessage = "El alumno Juan Perez ha obtenido un promedio de "
+				+ "6.33"
+				+  ". Puedes mejorar.";
+		//arrange
 		SubjectDTO subjectOne = new SubjectDTO("matematica",9.0);
 		SubjectDTO subjectTwo = new SubjectDTO("lengua", 0.0);
 		SubjectDTO subjectThree = new SubjectDTO("fisica", 10.0);
 		subjects.add(subjectOne);
 		subjects.add(subjectTwo);
 		subjects.add(subjectThree);
-		student = new StudentDTO();
-		student.setId(1L);
-		student.setStudentName("Juan Perez");
 		student.setSubjects(subjects);
-
+		//act
+		when(studentDao.findById(1L)).thenReturn(student);
+		StudentDTO result = diplomaService.analyzeScores(1L);
+		//asserts
+		Assertions.assertEquals(result.getAverageScore(), (double) 19/3);
+		Assertions.assertEquals(result.getMessage(), expectedMessage);
 	}
 
 	@Test
-	@DisplayName("Juan tiene promedio 6.33")
-	void TestObtenerDiplomaService(){
-
+	@DisplayName("Juan tiene promedio 10 y obtiene sus felicitaciones")
+	void TestObtenerDiplomaSobresaliente(){
 		//arrange
-		when(studentDao.findById(1L)).thenReturn(student);
+		String expectedMessage = "El alumno Juan Perez ha obtenido un promedio de "
+				+ new DecimalFormat("#.##").format(10)
+				+  ". Felicitaciones!";
+		SubjectDTO subjectOne = new SubjectDTO("matematica",10.0);
+		SubjectDTO subjectTwo = new SubjectDTO("lengua", 10.0);
+		SubjectDTO subjectThree = new SubjectDTO("fisica", 10.0);
+		subjects.add(subjectOne);
+		subjects.add(subjectTwo);
+		subjects.add(subjectThree);
+		student.setSubjects(subjects);
 		//act
+		when(studentDao.findById(1L)).thenReturn(student);
 		StudentDTO result = diplomaService.analyzeScores(1L);
 		//asserts
-
-		Assertions.assertEquals(result.getAverageScore(), (double) 19/3);
-
+		Assertions.assertEquals(result.getAverageScore(), 10);
+		Assertions.assertEquals(result.getMessage(), expectedMessage);
 	}
-
 }
