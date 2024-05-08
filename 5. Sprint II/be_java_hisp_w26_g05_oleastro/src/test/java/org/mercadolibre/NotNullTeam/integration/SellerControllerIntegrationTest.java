13894589ce45c2a9648f69de09c7a4d2aba8d20c
Lautaro Oleastro <lautaro.oleastro@mercadolibre.com>
2024-05-08 19:23:31 -0300

    Se agrego test de integracion getFollowersCountSuccessfully()

--- 5. Sprint II/be_java_hisp_w26_g05_oleastro/src/test/java/org/mercadolibre/NotNullTeam/integration/SellerControllerIntegrationTest.java
+++ 5. Sprint II/be_java_hisp_w26_g05_oleastro/src/test/java/org/mercadolibre/NotNullTeam/integration/SellerControllerIntegrationTest.java
@@ -1,23 +1,88 @@
 package org.mercadolibre.NotNullTeam.integration;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mercadolibre.NotNullTeam.DTO.response.seller.SellerFollowersCountDto;
+import org.mercadolibre.NotNullTeam.model.Buyer;
+import org.mercadolibre.NotNullTeam.model.Seller;
+import org.mercadolibre.NotNullTeam.model.User;
+import org.mercadolibre.NotNullTeam.repository.IBuyerRepository;
+import org.mercadolibre.NotNullTeam.repository.ISellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SellerControllerIntegrationTest {
-
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    IBuyerRepository buyerRepository;
+    @Autowired
+    ISellerRepository sellerRepository;
+    ObjectMapper objectMapper = new ObjectMapper();
+    List<Buyer> initialBuyers;
+    List<Seller> initialSellers;
+
+    @BeforeEach
+    public void setup() {
+        objectMapper.findAndRegisterModules();
+        ObjectWriter objectWriter = new ObjectMapper().
+                configure(SerializationFeature.WRAP_ROOT_VALUE, false).
+                writer();
+
+        Buyer buyerOne = Buyer.builder()
+                .user(User.builder().id(1L).name("Buyer One").build())
+                .followedList(new ArrayList<>())
+                .build();
+
+        Buyer buyerTwo = Buyer.builder()
+                .user(User.builder().id(2L).name("Buyer Two").build())
+                .followedList(new ArrayList<>())
+                .build();
+
+        Seller sellerOne = Seller.builder()
+                .user(User.builder().id(101L).name("Seller One").build())
+                .followersList(new ArrayList<>())
+                .build();
+
+        Seller sellerTwo = Seller.builder()
+                .user(User.builder().id(102L).name("Seller Two").build())
+                .followersList(new ArrayList<>())
+                .build();
+
+        buyerOne.addNewFollowed(sellerOne);
+        sellerOne.addNewFollower(buyerOne);
+        buyerOne.addNewFollowed(sellerTwo);
+        sellerTwo.addNewFollower(buyerOne);
+
+        initialBuyers = new ArrayList<>();
+        initialBuyers.add(buyerOne);
+        initialBuyers.add(buyerTwo);
+
+        initialSellers = new ArrayList<>();
+        initialSellers.add(sellerOne);
+        initialSellers.add(sellerTwo);
+
+        buyerRepository.setBuyers(initialBuyers);
+        sellerRepository.setSellers(initialSellers);
+    }
 
     @Test
     @DisplayName("Se quiere listar los seguidores del usuario vendedor con ID = 9999 " +
@@ -30,5 +95,34 @@ public class SellerControllerIntegrationTest {
                         jsonPath("$.message")
                         .value("No se encontro el vendedor con ID = 9999 not found")
                 );
+    }
+
+    @Test
+    @DisplayName("Se solicita el conteo de seguidores del usuario vendedor" +
+            "con ID = 101 que corresponde al usuario llamado Seller One " +
+            "y se obtiene que tiene un seguidor")
+    public void getFollowersCountSuccessfully() throws Exception {
+        SellerFollowersCountDto expectedSellerFollowersCountDto =
+                SellerFollowersCountDto.builder()
+                        .user_id(101L)
+                        .user_name("Seller One")
+                        .followers_count(1)
+                        .build();
+
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders
+                        .get("/users/{userId}/followers/count", 101)
+                )
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        SellerFollowersCountDto actualSellerFollowersCountDto = objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                new TypeReference<SellerFollowersCountDto>(){}
+        );
+
+        assertEquals(expectedSellerFollowersCountDto, actualSellerFollowersCountDto);
     }
 }
