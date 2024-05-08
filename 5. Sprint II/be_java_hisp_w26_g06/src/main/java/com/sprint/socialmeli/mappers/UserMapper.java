7ee15022441a09f31693e6d1c9f89fb0a61d3a03
Matias Pinto <matiasdaniel.pinto@mercadolibre.com>
2024-05-08 10:49:01 -0300

    feat: group SocialMeli2 - testing

---
+++ 5. Sprint II/be_java_hisp_w26_g06/src/main/java/com/sprint/socialmeli/mappers/UserMapper.java
@@ -0,0 +1,60 @@
+package com.sprint.socialmeli.mappers;
+
+import com.sprint.socialmeli.dto.user.FollowedResponseDTO;
+import com.sprint.socialmeli.dto.user.FollowerCountResponseDTO;
+import com.sprint.socialmeli.dto.user.FollowersResponseDTO;
+import com.sprint.socialmeli.dto.user.UserResponseDTO;
+import com.sprint.socialmeli.entity.Customer;
+import com.sprint.socialmeli.entity.IUser;
+import com.sprint.socialmeli.entity.Seller;
+import com.sprint.socialmeli.entity.User;
+
+import java.util.List;
+
+public class UserMapper {
+
+    public static FollowersResponseDTO mapUserToFollowerResponseDto(Seller seller, List<UserResponseDTO> usersDto) {
+        return new FollowersResponseDTO(
+                seller.getUser().getUserId(),
+                seller.getUser().getUserName(),
+                usersDto
+        );
+    }
+
+    public static FollowerCountResponseDTO mapUserToFollowerCountDto(Seller seller, Integer count) {
+        return new FollowerCountResponseDTO(
+                seller.getUser().getUserId(),
+                seller.getUser().getUserName(),
+                count
+        );
+    }
+
+    public static FollowedResponseDTO mapUserToFollowedResponseDto(Customer customer, List<UserResponseDTO> usersDto) {
+        return new FollowedResponseDTO(
+                customer.getUser().getUserId(),
+                customer.getUser().getUserName(),
+                usersDto
+        );
+    }
+
+    public static UserResponseDTO mapSellerToUserResponseDto(Seller seller){
+        return new UserResponseDTO(
+                seller.getUser().getUserId(),
+                seller.getUser().getUserName()
+        );
+    }
+
+    public static UserResponseDTO mapCustomerToUserResponseDto(Customer customer){
+        return new UserResponseDTO(
+                customer.getUser().getUserId(),
+                customer.getUser().getUserName()
+        );
+    }
+
+    public static UserResponseDTO mapUserToUserResponseDto(IUser user){
+        return new UserResponseDTO(
+                user.getUser().getUserId(),
+                user.getUser().getUserName()
+        );
+    }
+}
