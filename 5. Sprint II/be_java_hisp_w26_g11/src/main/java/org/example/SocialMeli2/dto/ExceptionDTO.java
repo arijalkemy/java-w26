dda9278123f7484eabcc0f6d14bd29415abc79f0
parriagafaja <pedro.arriagafajardo@mercadolibre.com.mx>
2024-05-08 13:33:59 -0600

    UPDATE: change name SocialMeli2

---
+++ 5. Sprint II/be_java_hisp_w26_g11/src/main/java/org/example/SocialMeli2/dto/ExceptionDTO.java
@@ -0,0 +1,13 @@
+package org.example.SocialMeli2.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionDTO {
+    private String message;
+}
