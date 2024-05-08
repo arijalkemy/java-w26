4790a2259bb56a478b711bf2123bbf13473bf9a1
Cesar Roman <cesar.romanzuniga@mercadolibre.com.mx>
2024-05-08 09:40:43 -0600

    ADD: Sprint 2 Modulo Grupal

---
+++ 4. Testing/GestorViajes/src/test/java/com/viajescuidados/viajescuidados/services/ViajeServiceTest.java
@@ -0,0 +1,2 @@
+package com.viajescuidados.viajescuidados.services;public class ViajeServiceTest {
+}
---
+++ 5. Sprint II/be_java_hisp_w26_g11/src/main/java/org/example/sprint1/entity/Post.java
@@ -0,0 +1,31 @@
+package org.example.sprint1.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder({"postId", "date", "product", "category", "price", "hasPromo", "discount"})
+@Builder
+public class Post {
+    @JsonProperty("post_id")
+    private int postId;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate date;
+    private Product product;
+    private int category;
+    private double price;
+    @JsonProperty("has_promo")
+    private boolean hasPromo;
+    private double discount;
+}
