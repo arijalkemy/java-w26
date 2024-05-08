ca1473a9378329bfb6f8430668832a169cba4aa6
dbellol <dbellol@unal.edu.co>
2024-05-08 18:41:54 -0500

    Proyecto grupal y tests: individual y al 75% o mas

---
+++ 5. Sprint II/be_java_hisp_w26_g08/social_meli/src/main/java/org/example/social_meli/repository/IProductRepository.java
@@ -0,0 +1,11 @@
+package org.example.social_meli.repository;
+
+import org.example.social_meli.model.Post;
+
+import java.util.List;
+
+public interface IProductRepository {
+    void savePost(Post post);
+    Boolean existsPost(Integer postId);
+    List<Post> getAllPosts();
+}
---
+++ 5. Sprint II/be_java_hisp_w26_g08_bello/social_meli/src/test/java/org/example/social_meli/controller/ProductControllerTest.java
@@ -0,0 +1,36 @@
+package org.example.social_meli.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.social_meli.dto.PostDTO;
+import org.example.social_meli.dto.ProductDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.print.attribute.standard.PrinterMessageFromOperator;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("Crear un post de un producto correctamente")
+    void postProductPost() throws Exception {
+        PostDTO postDTO = PostDTO.builder().post_id(1012).user_id(2).date(LocalDate.now()).product(ProductDTO.builder().product_id(1).product_name("Sol solecito").type("Tipo tipo").brand("Ford").color("Azul").notes("Sol solecito").build()).category(4).price(20.00).build();
+        mockMvc.perform(MockMvcRequestBuilders.post("/products/post").content(objectMapper.writeValueAsString(postDTO)).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(postDTO))).andExpect(MockMvcResultMatchers.jsonPath("$.user_id").value(2));
+    }
+}
