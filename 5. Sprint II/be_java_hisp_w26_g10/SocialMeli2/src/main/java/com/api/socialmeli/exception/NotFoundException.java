8f4b5b4b6c914464829b54f719b9c0b8c702b4d5
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-05-08 14:54:50 -0500

    Fixed the final project structure

--- 5. Sprint II/be_java_hisp_w26_g10/SocialMeli2/src/main/java/com/api/socialmeli/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.api.socialmeli.exception;
-
-public class NotFoundException  extends  RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- 5. Sprint II/be_java_hisp_w26_g10/SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/controller/SocialMeliControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.meli.be_java_hisp_w26_g10.controller;
-
-public class SocialMeliControllerTest {
-}
