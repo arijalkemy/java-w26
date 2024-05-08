3f65cfaea95a3c81480b2592ee0a31fa321eafa5
jccaserio <juancruz.caserio@mercadolibre.com>
2024-05-08 12:24:12 -0300

    sprint2: refactor and more test

--- 5. Sprint II/be_java_hisp_w26_g06-caserio/src/test/java/com/sprint/socialmeli/integration/FollowTest.java
+++ 5. Sprint II/be_java_hisp_w26_g06-caserio/src/test/java/com/sprint/socialmeli/integration/FollowTest.java
@@ -3,14 +3,13 @@ package com.sprint.socialmeli.integration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.sprint.socialmeli.exception.ConflictException;
+import com.sprint.socialmeli.dto.user.FollowerCountResponseDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -24,44 +23,62 @@ public class FollowTest {
     @Autowired
     private MockMvc mockMvc;
 
+    FollowerCountResponseDTO followerCount;
     private static ObjectWriter writer;
-
-    private static ObjectMapper objectMapper;
 
     @BeforeAll
     public static void setup() {
         writer = new ObjectMapper()
                 .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
                 .writer();
-
-        objectMapper = new ObjectMapper();
     }
 
     /**
      * Se testea integralmente la funcionalidad de follow, casos:
+     *  - Follow con sellerId inválido (debe arrojar not found)
+     *  - Follow con customerId inválido (debe arrojar not found)
      *  - Follow entre customerId2 y sellerId1
      *  - Follow de nuevo entre ya seguidos (debe arrojar conflicto)
      *  - Unfollow entre customerId2 y sellerId1
      *  - Follow nuevamente entre customerId2 y sellerId1 (ahora si debe funcionar por el unfollow realizado)
+     *  - Obtener cantidad de seguidores de sellerId 1
      * @throws Exception
      */
     @Test
     public void testFollow() throws Exception {
-        //El Customer con id 2 sigue al Seller con id 1
+
+        // El customerId 2 quiere seguir nuevamente al sellerId 100
+        // Debe lanzar not found
+        ResultActions result = this.mockMvc.perform(
+                MockMvcRequestBuilders.post("/users/2/follow/100"));
+        result.andExpect(status().isNotFound());
+
+        // El customerId 200 quiere seguir nuevamente al sellerId 1
+        // Debe lanzar not found
+        result = this.mockMvc.perform(
+                MockMvcRequestBuilders.post("/users/200/follow/1"));
+        result.andExpect(status().isNotFound());
+
+        //El customerId 2 sigue al sellerId 1
         simpleFollowTest();
 
-        // El customer con id 2 quiere seguir nuevamente al seller con id 1
+        // El customerId 2 quiere seguir nuevamente al sellerId 1
         // Debe lanzar conflicto
-        ResultActions result = this.mockMvc.perform(
+        result = this.mockMvc.perform(
                 MockMvcRequestBuilders.post("/users/2/follow/1"));
-
         result.andExpect(status().isConflict());
 
-        //El Customer con id 2 deja de seguir al Seller con id 1
+        //El customerId 2 deja de seguir al sellerId 1
         simpleUnfollowTest();
 
-        //El Customer con id 2 sigue al Seller con id 1
+        //sellerId 1 no tiene seguidores
+        checkFollowerCount(0);
+
+        //El customerId 2 sigue al sellerId 1
         simpleFollowTest();
+
+        //sellerId 1 tiene 1 seguidor
+        checkFollowerCount(1);
     }
 
     private void simpleFollowTest() throws Exception {
@@ -80,5 +97,15 @@ public class FollowTest {
         result.andExpect(status().isOk())
                 .andExpect(content().contentType("text/plain;charset=UTF-8"))
                 .andExpect(content().string("OK"));
+    }
+
+    private void checkFollowerCount( int count) throws Exception {
+        ResultActions result = this.mockMvc.perform(
+                MockMvcRequestBuilders.get("/users/1/followers/count"));
+        result.andExpect(status().isOk());
+        FollowerCountResponseDTO followerCount = new FollowerCountResponseDTO(1, "Martin",count);
+        String respuestaEsperadaString = writer.writeValueAsString(followerCount);
+
+        Assertions.assertEquals(respuestaEsperadaString, result.andReturn().getResponse().getContentAsString());
     }
 }
