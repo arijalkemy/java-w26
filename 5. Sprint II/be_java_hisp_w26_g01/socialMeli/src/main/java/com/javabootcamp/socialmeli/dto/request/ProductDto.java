795fb2eb8d6b52918eb90adaa6d8692aebf01b3e
JmCoroMeli <juanmanuel.coro@mercadolibre.com>
2024-05-08 12:58:44 -0300

    fix productDto group part

--- 5. Sprint II/be_java_hisp_w26_g01/socialMeli/src/main/java/com/javabootcamp/socialmeli/dto/request/ProductDto.java
+++ 5. Sprint II/be_java_hisp_w26_g01/socialMeli/src/main/java/com/javabootcamp/socialmeli/dto/request/ProductDto.java
@@ -19,22 +19,22 @@ public class ProductDto {
     @JsonProperty("product_name")
     @Size( max = 40, message = "La longitud no puede superar los 40 caracteres.")
     @NotBlank(message = "El campo no puede estar vacío.")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "El campo no puede poseer caracteres especiales.")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
     private String name;
 
     @Size(max = 15, message = "La longitud no puede superar los 15 caracteres.")
     @NotBlank(message = "El campo no puede estar vacío.")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "El campo no puede poseer caracteres especiales.")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
     private String type;
 
     @Size(max = 25, message = "La longitud no puede superar los 25 caracteres.")
     @NotBlank(message = "El campo no puede estar vacío.")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "El campo no puede poseer caracteres especiales.")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
     private String brand;
 
     @Size(max = 15, message = "La longitud no puede superar los 15 caracteres.")
     @NotBlank(message = "El campo no puede estar vacío.")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "El campo no puede poseer caracteres especiales.")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
     private String color;
 
     @Size(max = 80, message = "La longitud no puede superar los 80 caracteres.")
