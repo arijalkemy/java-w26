f7e815b7b8490ac38f45bc35760559c781f95fe9
parriagafaja <pedro.arriagafajardo@mercadolibre.com.mx>
2024-05-08 12:24:38 -0600

    ADD: test sprint 2

---
+++ 5. Sprint II/be_java_hisp_w26_g11_arriaga/src/main/java/org/example/sprint1/dto/BasicSellerDTO.java
@@ -0,0 +1,32 @@
+package org.example.sprint1.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.sprint1.entity.Post;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasicSellerDTO implements Serializable {
+    @JsonProperty("user_id")
+    @JsonAlias("seller_id")
+    private int sellerId;
+
+    @JsonProperty("user_name")
+    @JsonAlias("seller_name")
+    private String sellerName;
+
+    @JsonIgnore
+    private List<Post> posts;
+
+    @JsonIgnore
+    private List<Integer> followers;
+
+}
---
+++ 5. Sprint II/be_java_hisp_w26_g11_arriaga/src/main/java/org/example/sprint1/entity/Customer.java
@@ -0,0 +1,27 @@
+package org.example.sprint1.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+    @JsonProperty("user_id")
+    private int userId;
+    @JsonProperty("user_name")
+    private String userName;
+    @JsonProperty("following")
+    private List<Integer> sellers;
+
+    public void addSeller(int sellerId) {
+        sellers.add(sellerId);
+    }
+}
