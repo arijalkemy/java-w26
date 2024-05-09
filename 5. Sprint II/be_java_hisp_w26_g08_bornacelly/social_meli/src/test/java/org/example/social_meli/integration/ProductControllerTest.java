f4cd8b0019d9ce90d4ea29c6583274dc88119e70
abornacelly <aldo.bornacelly@mercadolibre.com.co>
2024-05-09 13:47:39 -0500

    Se agrega el desarrollo individual con el bonus

---
+++ 5. Sprint II/be_java_hisp_w26_g08_bornacelly/social_meli/src/test/java/org/example/social_meli/integration/ProductControllerTest.java
@@ -0,0 +1,108 @@
+package org.example.social_meli.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.example.social_meli.dto.ExceptionDTO;
+import org.example.social_meli.dto.PostDTO;
+import org.example.social_meli.dto.ProductDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    private static ObjectMapper objectMapper;
+
+    private static ObjectWriter objectWriter;
+
+    @BeforeAll
+    public static void setUp(){
+        objectWriter = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+
+        objectMapper = new ObjectMapper();
+    }
+
+    @DisplayName("Se verifica la integración del sistema en la creacion de un post")
+    @Test
+    void postProductPostTest() throws Exception {
+        Random rand = new Random();
+
+        int min = 200;
+        int max = 1000;
+        int postId = rand.nextInt((max - min) + 1) + min;
+
+        ProductDTO product = new ProductDTO(1, "product A", "Type A", "Brand A", "Blue", "note");
+        LocalDate postLocalDate = LocalDate.of(2024, 5, 6);
+        PostDTO postDto = new PostDTO(postId, 2, postLocalDate, product, 1, 12000.0);
+
+        ResultActions results = mockMvc.perform(
+                MockMvcRequestBuilders.post("/products/post")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectWriter.writeValueAsString(postDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk()
+        );
+
+        String resultString = results.andReturn().getResponse().getContentAsString();
+        String expectedString = objectMapper.writeValueAsString(postDto);
+
+        Assertions.assertEquals(expectedString, resultString);
+    }
+
+    @DisplayName("Se debería obtener un mensaje indicando que ya existe un post con id 1")
+    @Test
+    void postProductPostWithExistPostIdTest() throws Exception {
+
+        ProductDTO product = new ProductDTO(1, "product A", "Type A", "Brand A", "Blue", "note");
+        LocalDate postLocalDate = LocalDate.of(2024, 5, 6);
+        PostDTO postDto = new PostDTO(1, 2, postLocalDate, product, 1, 12000.0);
+
+        ExceptionDTO message = new ExceptionDTO("Ya existe un post con el id 1");
+
+        ResultActions results = mockMvc.perform(
+                        MockMvcRequestBuilders.post("/products/post")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectWriter.writeValueAsString(postDto)))
+                .andExpect(MockMvcResultMatchers.status().isConflict()
+                );
+
+        String resultString = results.andReturn().getResponse().getContentAsString();
+        String expectedString = objectMapper.writeValueAsString(message);
+        Assertions.assertEquals(expectedString, resultString);
+    }
+
+    @DisplayName("Se debería obtener la lista de seguidos del usuario con id 1")
+    @Test
+    void getSellersPostsFollowedByUser() throws Exception {
+        Integer userId = 1;
+        String url = String.format("/products/followed/%d/list", userId);
+
+        ResultActions results = mockMvc.perform(
+                MockMvcRequestBuilders.get(url))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.userId").value(1)
+
+        );
+    }
+
+}
---
+++ 5. Sprint II/be_java_hisp_w26_g08_bornacelly/social_meli/src/test/java/org/example/social_meli/integration/UserControllerTest.java
@@ -0,0 +1,66 @@
+package org.example.social_meli.integration;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.social_meli.dto.FollowListDTO;
+import org.example.social_meli.dto.UserResponseDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("Deberia dejar de seguir a un usuario")
+    void postUnfollowUser() throws Exception {
+        Integer userId = 1;
+        Integer userIdToUnfollow = 2;
+
+        String url = String.format("/users/%d/unfollow/%d", userId, userIdToUnfollow);
+
+        UserResponseDTO userResponseDTO = new UserResponseDTO(1,"wcalderwood0", List.of());
+
+        ResultActions results = mockMvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        String resultString = results.andReturn().getResponse().getContentAsString();
+        String expectedString = objectMapper.writeValueAsString(userResponseDTO);
+
+        Assertions.assertEquals(expectedString, resultString);
+    }
+
+    @Test
+    @DisplayName("Deberia permitir al usuario con id 3 seguir al 2")
+    void postfollowUserTest() throws Exception {
+        Integer userId = 3;
+        Integer userIdTofollow = 2;
+
+        FollowListDTO followList = new FollowListDTO();
+        String url = String.format("/users/%d/follow/%d", userId, userIdTofollow);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post(url))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
