5a525ca95d433c4f1562bf03a32237591ac5ca24
dbellol <dbellol@unal.edu.co>
2024-05-09 12:46:09 -0500

    Modificacion Builders tests as de  75

---
+++ 5. Sprint II/be_java_hisp_w26_g08_bello/social_meli/src/main/java/org/example/social_meli/model/User.java
@@ -0,0 +1,16 @@
+package org.example.social_meli.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer user_id;
+    private String user_name;
+    private Boolean isSeller;
+}
---
+++ 5. Sprint II/be_java_hisp_w26_g08_bello/social_meli/src/main/java/org/example/social_meli/repository/IUserRepository.java
@@ -0,0 +1,22 @@
+package org.example.social_meli.repository;
+
+import org.example.social_meli.model.FollowerList;
+import org.example.social_meli.model.User;
+
+
+public interface IUserRepository {
+    Integer getClientIndex(FollowerList client);
+    Integer getSellerIndex(FollowerList seller);
+    User findById(Integer user_id);
+    FollowerList findSellerById(Integer id);
+    FollowerList findClientById(Integer id);
+    FollowerList findSellerByUser(User user);
+    FollowerList findClientByUser(User user);
+    Boolean existsById(Integer user_Id);
+    Boolean existsClientById(Integer clientId);
+    Boolean existsSellerById(Integer sellerId);
+    void updateClients(Integer index, FollowerList client);
+    void updateSellers(Integer index, FollowerList seller);
+    void saveSeller(FollowerList seller);
+    void saveClient(FollowerList client);
+}
