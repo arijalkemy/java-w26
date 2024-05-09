903121712b42bd1fe2c138ea19a876b1b1b8cced
ctorreschapa <cristianandres.torreschaparro@mercadolibre.com.co>
2024-05-09 14:49:55 -0500

    Sprint 2

---
+++ 5. Sprint II/be_java_hisp_w26_g07/src/main/java/org/example/be_java_hisp_w26_g07/entity/Product.java
@@ -0,0 +1,24 @@
+package org.example.be_java_hisp_w26_g07.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @JsonProperty("product_id")
+    private Integer id;
+    @JsonProperty("product_name")
+    private String name;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("brand")
+    private String brand;
+    @JsonProperty("color")
+    private String color;
+    @JsonProperty("notes")
+    private String notes;
+}
