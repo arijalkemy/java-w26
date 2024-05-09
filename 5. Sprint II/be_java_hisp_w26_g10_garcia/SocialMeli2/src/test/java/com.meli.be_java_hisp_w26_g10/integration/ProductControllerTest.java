d260ae2663387bd7e3a484fdc076a5f4dab0d31b
John Stiven Garcia Vargas <johnstiven.garciavargas@mercadolibre.com.co>
2024-05-09 15:59:48 -0500

    Agrego el primer test de integración de la creación de un post

--- 5. Sprint II/be_java_hisp_w26_g10_garcia/SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/integration/ProductControllerTest.java
+++ 5. Sprint II/be_java_hisp_w26_g10_garcia/SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/integration/ProductControllerTest.java
@@ -1,21 +1,31 @@
 package com.meli.be_java_hisp_w26_g10.integration;
 
-import com.api.socialmeli.controller.ProductController;
-import com.api.socialmeli.service.impl.PostServiceImpl;
+import com.api.socialmeli.BeJavaHispW26G10Application;
+import com.api.socialmeli.dto.PostDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.meli.be_java_hisp_w26_g10.util.TestGeneratorUtil;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(classes = BeJavaHispW26G10Application.class)
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
 public class ProductControllerTest {
@@ -23,17 +33,35 @@ public class ProductControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Mock
-    private PostServiceImpl postService;
-
-    @InjectMocks
-    private ProductController productController;
-
-    private ObjectMapper mapper = new ObjectMapper();
+    private ObjectMapper mapper;
 
     @BeforeEach
     void setUp() {
         mapper = new ObjectMapper().registerModule(new JavaTimeModule());
     }
+
+    @Test
+    @DisplayName("Se crea un Post de forma exitosa")
+    public void CreatePostTest() throws Exception {
+        //Arrange
+        PostDto postDto = mapper.convertValue(TestGeneratorUtil.postList().get(0), PostDto.class);
+        postDto.getProduct().setProduct_name("Smartphone");
+
+        ObjectWriter writer = mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer().withDefaultPrettyPrinter();
+
+        String payloadJson = writer.writeValueAsString(postDto);
+
+        //Act
+        //Assert
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/products/post")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(payloadJson))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
+
+    }
+
 
 }
