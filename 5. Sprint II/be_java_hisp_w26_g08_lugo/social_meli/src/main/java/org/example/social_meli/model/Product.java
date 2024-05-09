8c90d388e454d2670ea2fe835686191be62ab871
Anfeldev <andresfelipe.lugo@mercadolibre.com.co>
2024-05-09 12:32:00 -0500

    fix: Refactoriza anotaciones sin usar

--- 5. Sprint II/be_java_hisp_w26_g08_lugo/social_meli/src/main/java/org/example/social_meli/model/Product.java
+++ 5. Sprint II/be_java_hisp_w26_g08_lugo/social_meli/src/main/java/org/example/social_meli/model/Product.java
@@ -1,7 +1,6 @@
 package org.example.social_meli.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- 5. Sprint II/be_java_hisp_w26_g08_lugo/social_meli/src/main/java/org/example/social_meli/model/User.java
+++ 5. Sprint II/be_java_hisp_w26_g08_lugo/social_meli/src/main/java/org/example/social_meli/model/User.java
@@ -2,7 +2,6 @@ package org.example.social_meli.model;
 
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
