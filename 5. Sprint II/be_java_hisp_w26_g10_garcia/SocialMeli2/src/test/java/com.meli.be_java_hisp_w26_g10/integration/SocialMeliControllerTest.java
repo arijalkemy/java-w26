2a42784f4dd5a7f9f482c7b52a40c2dbc14a3ca0
John Stiven Garcia Vargas <johnstiven.garciavargas@mercadolibre.com.co>
2024-05-09 16:52:34 -0500

    Se agrega un test de integración para conocer el número de seguidores bonus

--- 5. Sprint II/be_java_hisp_w26_g10_garcia/SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/integration/SocialMeliControllerTest.java
+++ 5. Sprint II/be_java_hisp_w26_g10_garcia/SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/integration/SocialMeliControllerTest.java
@@ -1,12 +1,26 @@
 package com.meli.be_java_hisp_w26_g10.integration;
 
 import com.api.socialmeli.BeJavaHispW26G10Application;
+import com.api.socialmeli.dto.PostDto;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.meli.be_java_hisp_w26_g10.util.TestGeneratorUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = BeJavaHispW26G10Application.class)
 @AutoConfigureMockMvc
@@ -15,5 +29,24 @@ public class SocialMeliControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Se pide el número de segidores de un vendedor")
+    public void getFollowersCountTest() throws Exception {
+        //Arrange
+        Long userId = 1L;
+
+        //Act
+        //Assert
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/users/{userId}/followers/count",userId)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.followers_count")
+                        .value(4));
+
+    }
+
 
 }
