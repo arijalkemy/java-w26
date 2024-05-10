5e655ca1bb3d54b31540e9a4b32980725635d1b0
AkublerMeli <andres.kubler@mercadolibre.com>
2024-05-10 09:33:02 -0300

    Agrego

---
+++ 5. Sprint II/1.Grupal/SocialMeli-2/src/main/java/com/group03/sprint2/dto/response/UserDataResponseDTO.java
@@ -0,0 +1,16 @@
+package com.group03.sprint2.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDataResponseDTO {
+    @JsonProperty("user_id")
+    private Integer userId;
+    @JsonProperty("user_name")
+    private String userName;
+}
