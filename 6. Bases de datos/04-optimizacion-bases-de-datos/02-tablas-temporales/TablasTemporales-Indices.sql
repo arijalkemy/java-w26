c354798bb66a9090936d0ff1315e4b42194a4bca
ggomezr1403 <geraldine.gomezromero@mercadolibre.com.co>
2024-05-14 14:22:14 -0500

    Modulo BDs | Tablas temporales e indices - MoviesDB

---
+++ 6. Bases de datos/04-optimizacion-bases-de-datos/02-tablas-temporales/TablasTemporales-Indices.sql
@@ -0,0 +1,26 @@
+CREATE TEMPORARY TABLE TWD(title VARCHAR(100), rating DECIMAL(3,1), release_date DATETIME, serie_title VARCHAR(100));
+
+INSERT INTO TWD SELECT ep.title, rating, ep.release_date, sr.title FROM episodes ep
+JOIN seasons sns ON ep.season_id = sns.id
+JOIN series sr ON sns.serie_id = sr.id
+WHERE sr.title = 'The Walking dead';
+
+SELECT * FROM TWD;
+
+-- CREACIÓN DE INDICES --
+
+-- Mejora del rendimiento de consultas
+-- Cuando se realizan consultas que involucran la búsqueda por el nombre del actor,
+-- un índice en esa columna puede mejorar significativamente el rendimiento de esas consultas.
+
+CREATE INDEX actor_name_index ON actors(first_name);
+
+EXPLAIN SELECT * FROM actors WHERE first_name = "Daniel";
+
+-- Búsqueda rápida por título
+-- En lugar de tener que buscar secuencialmente en toda la tabla de películas, la base de datos puede utilizar el índice para 
+-- encontrar rápidamente las filas que coinciden con el título buscado.
+
+CREATE INDEX title_name_index ON movies(title);
+
+EXPLAIN SELECT * FROM movies WHERE title LIKE "Harry Potter%";
