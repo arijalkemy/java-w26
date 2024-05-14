dd57f0989bdc383ccd552e8b2782b2ccc4af50f9
Rodrigo Beltran <rodrigo.beltran@mercadolibre.com.co>
2024-05-14 11:05:29 -0500

    Ejercicios de clase asincronica

---
+++ 6. Bases de datos/biblioteca_db_consultas.sql
@@ -0,0 +1,175 @@
+# EJERCICIO 2
+
+CREATE SCHEMA IF NOT EXISTS libros_db;
+USE libros_db;
+
+DROP TABLE IF EXISTS autor;
+
+CREATE TABLE autor (
+	idAutor INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(60) NOT NULL,
+    Nacionalidad VARCHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS estudiante;
+
+CREATE TABLE estudiante(
+	idLector INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(60) NOT NULL,
+    Apellido VARCHAR(60) NOT NULL,
+    Direccion VARCHAR(100),
+    Carrera VARCHAR(100),
+    Edad INT
+);
+
+DROP TABLE IF EXISTS libro;
+
+CREATE TABLE libro(
+	idLibro INT AUTO_INCREMENT PRIMARY KEY,
+	Titulo VARCHAR(150) NOT NULL,
+    Editorial VARCHAR(60) NOT NULL,
+    Area VARCHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS libroautor;
+
+CREATE TABLE libroautor(
+    idAutor INT NOT NULL,
+    idLibro INT NOT NULL,
+    PRIMARY KEY (idAutor, idLibro),
+    FOREIGN KEY (idAutor) REFERENCES autor(idAutor),
+    FOREIGN KEY (idLibro) REFERENCES libro(idLibro)
+);
+
+DROP TABLE IF EXISTS prestamo;
+
+CREATE TABLE prestamo(
+	idLector INT NOT NULL,
+    idLibro INT NOT NULL,
+    FechaPrestamo DATE NOT NULL,
+    FechaDevolucion DATE NOT NULL,
+    Devuelto BOOLEAN NOT NULL DEFAULT(false),
+    PRIMARY KEY (idLector, idLibro),
+    FOREIGN KEY (idLector) REFERENCES estudiante(idLector),
+    FOREIGN KEY (idLibro) REFERENCES libro(idLibro)
+);
+
+-- Insertar autores
+INSERT INTO autor (Nombre, Nacionalidad) VALUES
+    ('Gabriel García Márquez', 'Colombiano'),
+    ('J.K. Rowling', 'Británica'),
+    ('Haruki Murakami', 'Japonés'),
+    ('Isabel Allende', 'Chilena'),
+    ('Stephen King', 'Estadounidense');
+
+-- Insertar estudiantes
+INSERT INTO estudiante (Nombre, Apellido, Direccion, Carrera, Edad) VALUES
+    ('Ana', 'Gómez', 'Calle 123, Bogotá', 'Informatica', 20),
+    ('Carlos', 'Pérez', 'Av. Principal, Medellín', 'Medicina', 22),
+    ('Laura', 'Rodríguez', 'Carrera 7, Cali', 'Derecho', 21),
+    ('Pedro', 'Martínez', 'Calle 10, Barranquilla', 'Economía', 23),
+    ('María', 'López', 'Calle 5, Cartagena', 'Arquitectura', 19);
+
+-- Insertar libros
+INSERT INTO libro (Titulo, Editorial, Area) VALUES
+    ('Cien años de soledad', 'Sudamericana', 'Realismo mágico'),
+    ('Harry Potter y la piedra filosofal', 'Bloomsbury', 'Fantasía'),
+    ('Tokio Blues', 'Kodansha', 'Contemporánea'),
+    ('La casa de los espíritus', 'Plaza & Janés', 'Realismo mágico'),
+    ('It', 'Salamandra', 'Terror'),
+	('El Universo: Guía de viaje', 'Salamandra', 'Fantasia');
+
+-- Asociar autores con libros
+INSERT INTO libroautor (idAutor, idLibro) VALUES
+    (1, 1), -- Gabriel García Márquez con "Cien años de soledad"
+    (2, 2), -- J.K. Rowling con "Harry Potter y la piedra filosofal"
+    (3, 3), -- Haruki Murakami con "Tokio Blues"
+    (4, 4), -- Isabel Allende con "La casa de los espíritus"
+    (5, 5), -- Stephen King con "It"
+	(1, 6),
+    (2, 6);
+    
+-- Registrar préstamos
+INSERT INTO prestamo (idLector, idLibro, FechaPrestamo, FechaDevolucion, Devuelto) VALUES
+    (1, 1, '2024-05-14', '2024-06-14', true),
+    (2, 3, '2024-05-15', '2024-06-15', false),
+    (3, 2, '2024-05-16', '2024-06-16', false),
+    (4, 4, '2024-05-17', '2024-06-17', true),
+    (5, 5, '2024-05-18', '2024-06-18', false);
+
+# EJERCICIO 1
+
+# 1. Listar los datos de los autores.
+SELECT *
+FROM autor;
+
+# 2. Listar nombre y edad de los estudiantes
+SELECT Nombre, Edad
+FROM estudiante;
+
+# 3. ¿Qué estudiantes pertenecen a la carrera informática?
+SELECT *
+FROM estudiante
+WHERE Carrera = 'Informatica';
+
+# 4. ¿Qué autores son de nacionalidad francesa o italiana?
+SELECT *
+FROM autor
+WHERE Nacionalidad = 'Francesa' OR Nacionalidad = 'Italiana';
+
+# 5. ¿Qué libros no son del área de internet?
+SELECT *
+FROM libro
+WHERE Area != 'Internet';
+
+# 6. Listar los libros de la editorial Salamandra.
+SELECT *
+FROM libro
+WHERE Editorial = 'Salamandra';
+
+# 7. Listar los datos de los estudiantes cuya edad es mayor al promedio.
+SELECT *
+FROM estudiante
+WHERE Edad > (SELECT AVG(Edad) FROM estudiante);
+
+# 8. Listar los nombres de los estudiantes cuyo apellido comience con la letra G.
+SELECT Nombre, Apellido
+FROM estudiante
+WHERE Apellido Like 'G%';
+
+# 9. Listar los autores del libro “El Universo: Guía de viaje”. (Se debe listar solamente los nombres).
+SELECT Nombre
+FROM autor a 
+INNER JOIN libroautor la ON a.idAutor=la.idAutor
+INNER JOIN libro l ON l.idLibro = la.idLibro
+WHERE l.Titulo='El Universo: Guía de viaje';
+
+# 10. ¿Qué libros se prestaron al lector “Filippo Galli”?
+SELECT l.Titulo, l.Editorial
+FROM estudiante e 
+INNER JOIN prestamo p ON e.idLector = p.idLector
+INNER JOIN libro l ON l.idLibro = p.idLibro
+WHERE e.Nombre = 'Filippo' AND e.Apellido = 'Galli';
+
+# 11. Listar el nombre del estudiante de menor edad.
+SELECT Nombre, Apellido, Edad
+FROM estudiante
+WHERE Edad < 18;
+
+# 12. Listar nombres de los estudiantes a los que se prestaron libros de Base de Datos.
+SELECT e.Nombre, e.Apellido
+FROM estudiante e 
+INNER JOIN prestamo p ON e.idLector = p.idLector
+INNER JOIN libro l ON l.idLibro = p.idLibro;
+
+# 13. Listar los libros que pertenecen a la autora J.K. Rowling.
+SELECT l.Titulo, l.Editorial
+FROM libro l 
+INNER JOIN libroautor la ON la.idLibro = l.idLibro
+INNER JOIN autor a ON a.idAutor = la.idAutor
+WHERE a.Nombre = 'J.K. Rowling';
+
+# 14. Listar títulos de los libros que debían devolverse el 16/07/2021.
+SELECT l.Titulo, l.Editorial
+FROM libro l INNER JOIN prestamo p ON p.idLibro = l.idLibro
+WHERE p.FechaDevolucion = '2021-07-16';
---
+++ 6. Bases de datos/empleado-consultas.sql
@@ -0,0 +1,85 @@
+CREATE SCHEMA IF NOT EXISTS empleado_db;
+
+USE empleado_db;
+
+DROP TABLE IF EXISTS departamento;
+
+CREATE TABLE departamento (
+	depto_nro VARCHAR(10),
+    nombre_depto VARCHAR(50),
+    localidad VARCHAR(50),
+    PRIMARY KEY (depto_nro)
+);
+
+DROP TABLE IF EXISTS empleado;
+
+CREATE TABLE empleado (
+    cod_emp VARCHAR(10),
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    puesto VARCHAR(50),
+    fecha_alta DATE,
+    salario INT,
+    comision INT,
+    depto_nro VARCHAR(10),
+    PRIMARY KEY (cod_emp),
+    FOREIGN KEY (depto_nro) REFERENCES departamento(depto_nro)
+);
+
+INSERT INTO departamento (depto_nro, nombre_depto, localidad) VALUES 
+('D-000-1', 'Software', 'Los Tigres'),
+('D-000-2', 'Sistemas', 'Guadalupe'),
+('D-000-3', 'Contabilidad', 'La Roca'),
+('D-000-4', 'Ventas', 'Plata');
+
+INSERT INTO empleado (cod_emp, nombre, apellido, puesto, fecha_alta, salario, comision, depto_nro) VALUES 
+('E-0001', 'César', 'Piñero', 'Vendedor', '2018-05-12', 80000, 15000, 'D-000-4'),
+('E-0002', 'Yosep', 'Kowaleski', 'Analista', '2015-07-14', 140000, 0, 'D-000-2'),
+('E-0003', 'Mariela', 'Barrios', 'Director', '2014-06-05', 185000, 0, 'D-000-3'),
+('E-0004', 'Jonathan', 'Aguilera', 'Vendedor', '2015-06-03', 85000, 10000, 'D-000-4'),
+('E-0005', 'Daniel', 'Brezecicki', 'Vendedor', '2018-03-03', 83000, 10000, 'D-000-4'),
+('E-0006', 'Mito', 'Barchuk', 'Presidente', '2014-06-05', 190000, 0, 'D-000-3'),
+('E-0007', 'Emilio', 'Galarza', 'Desarrollador', '2014-08-02', 60000, 0, 'D-000-1');
+
+# 1. Seleccionar el nombre, el puesto y la localidad de los departamentos donde trabajan los vendedores.
+SELECT e.nombre, e.apellido, e.puesto, d.localidad
+FROM departamento d
+INNER JOIN empleado e ON d.depto_nro = e.depto_nro
+WHERE e.puesto = 'Vendedor';
+
+# 2. Visualizar los departamentos con más de cinco empleados.
+SELECT COUNT(*), d.nombre_depto
+FROM departamento d
+INNER JOIN empleado e ON d.depto_nro = e.depto_nro
+GROUP BY d.depto_nro
+HAVING COUNT(*) > 5;
+
+# 3. Mostrar el nombre, salario y nombre del departamento de los empleados que tengan el mismo puesto que ‘Mito Barchuk’.
+SELECT e.nombre, e.salario, d.nombre_depto
+FROM empleado e
+INNER JOIN departamento d ON d.depto_nro = e.depto_nro
+WHERE e.puesto LIKE (SELECT puesto
+					FROM empleado
+                    WHERE nombre = 'Mito' AND apellido = 'Barchuk');
+                    
+# 4. Mostrar los datos de los empleados que trabajan en el departamento de contabilidad, ordenados por nombre.
+SELECT e.*
+FROM empleado e
+INNER JOIN departamento d ON d.depto_nro = e.depto_nro
+WHERE d.depto_nro LIKE (SELECT depto_nro
+						FROM departamento
+                        WHERE nombre_depto = 'Contabilidad');
+
+# 5. Mostrar el nombre del empleado que tiene el salario más bajo.
+SELECT nombre
+FROM EMPLEADO
+WHERE salario = (SELECT MIN(salario) FROM EMPLEADO);
+
+# 6. Mostrar los datos del empleado que tiene el salario más alto en el departamento de ‘Ventas’.
+SELECT *
+FROM EMPLEADO
+WHERE salario = (SELECT MAX(salario) 
+				FROM EMPLEADO 
+                WHERE depto_nro = (SELECT depto_nro 
+									FROM DEPARTAMENTO 
+									WHERE nombre_depto = 'Ventas'));
