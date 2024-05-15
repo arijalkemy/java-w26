9e63c5f2c971d720dc4713856f163294bc9dfdb7
levianfrancisco <levianlastra@gmail.com>
2024-05-15 14:23:49 -0300

    ejercicio_manejoDeBlogs

--- 2. Modulo Spring/ejercicio_manejoDeBlogs/src/main/java/com/ejercicio/manejodeblogs/exception/GlobalExceptionHandler.java
+++ 2. Modulo Spring/ejercicio_manejoDeBlogs/src/main/java/com/ejercicio/manejodeblogs/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ejercicio.manejodeblogs.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +20,6 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> NotFoundException(Exception e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
+
+
 }
