e6c8a83aa8af0505fc5120a6f7835e7e1c8a0cfb
John Stiven Garcia Vargas <johnstiven.garciavargas@mercadolibre.com.co>
2024-05-16 06:34:23 -0500

    Se agregan las consultas sobre bd empleados ejercicio en vivo

--- 6. Bases de datos/Consultas bd empleados.sql
+++ 6. Bases de datos/Consultas bd empleados.sql
@@ -40,3 +40,47 @@ INSERT INTO empleado (cod_emp, nombre, apellido, puesto, fecha_alta, salario, co
 ('E-0005', 'Daniel', 'Brezecicki', 'Vendedor', '2018-03-03', 83000, 10000, 'D-000-4'),
 ('E-0006', 'Mito', 'Barchuk', 'Presidente', '2014-06-05', 190000, 0, 'D-000-3'),
 ('E-0007', 'Emilio', 'Galarza', 'Desarrollador', '2014-08-02', 60000, 0, 'D-000-1');
+
+# 1. Seleccionar el nombre, el puesto y la localidad de los departamentos donde trabajan los vendedores.
+SELECT e.nombre, e.apellido, e.puesto, d.localidad
+FROM departamento d
+INNER JOIN empleado e ON d.depto_nro = e.depto_nro
+WHERE e.puesto = 'Vendedor';
+
+# 2. Visualizar los departamentos con más de cinco empleados.
+SELECT COUNT(*), d.nombre_depto
+FROM departamento d
+INNER JOIN empleado e ON d.depto_nro = e.depto_nro
+GROUP BY d.depto_nro
+HAVING COUNT(*) > 5;
+
+# 3. Mostrar el nombre, salario y nombre del departamento de los empleados que tengan el mismo puesto que ‘Mito Barchuk’.
+SELECT e.nombre, e.salario, d.nombre_depto
+FROM empleado e
+INNER JOIN departamento d ON d.depto_nro = e.depto_nro
+WHERE e.puesto LIKE (SELECT puesto
+					FROM empleado
+                    WHERE nombre = 'Mito' AND apellido = 'Barchuk');
+                    
+# 4. Mostrar los datos de los empleados que trabajan en el departamento de contabilidad, ordenados por nombre.
+SELECT e.*
+FROM empleado e
+INNER JOIN departamento d ON d.depto_nro = e.depto_nro
+WHERE d.depto_nro LIKE (SELECT depto_nro
+						FROM departamento
+                        WHERE nombre_depto = 'Contabilidad');
+
+# 5. Mostrar el nombre del empleado que tiene el salario más bajo.
+SELECT nombre
+FROM EMPLEADO
+WHERE salario = (SELECT MIN(salario) FROM EMPLEADO);
+
+# 6. Mostrar los datos del empleado que tiene el salario más alto en el departamento de ‘Ventas’.
+SELECT *
+FROM EMPLEADO
+WHERE salario = (SELECT MAX(salario) 
+				FROM EMPLEADO 
+                WHERE depto_nro = (SELECT depto_nro 
+									FROM DEPARTAMENTO 
+									WHERE nombre_depto = 'Ventas'));
+
