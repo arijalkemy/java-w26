a733c1c8ac2f12b81924a79cf42822091edaed16
ggomezr1403 <geraldine.gomezromero@mercadolibre.com.co>
2024-05-17 15:57:48 -0500

    Modulo Impl. BDs | Intro a JPA - MiniSeries - Anotaciones pendientes agregadas

--- 7. Implementacion de bases de datos/01-jpa-hibernate/01-introduccion-jpa-hibernate/01-miniseries-clase-vivo/src/main/java/org/ggomezr/miniseries/dto/MiniSerieDTO.java
+++ 7. Implementacion de bases de datos/01-jpa-hibernate/01-introduccion-jpa-hibernate/01-miniseries-clase-vivo/src/main/java/org/ggomezr/miniseries/dto/MiniSerieDTO.java
@@ -1,8 +1,12 @@
 package org.ggomezr.miniseries.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MiniSerieDTO {
     private String name;
     private Double rating;
--- 7. Implementacion de bases de datos/01-jpa-hibernate/01-introduccion-jpa-hibernate/01-miniseries-clase-vivo/src/main/java/org/ggomezr/miniseries/model/MiniSerie.java
+++ 7. Implementacion de bases de datos/01-jpa-hibernate/01-introduccion-jpa-hibernate/01-miniseries-clase-vivo/src/main/java/org/ggomezr/miniseries/model/MiniSerie.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class MiniSerie {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
