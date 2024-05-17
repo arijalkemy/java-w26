71a530672843850778f445837e16786a703abb5f
ggomezr1403 <geraldine.gomezromero@mercadolibre.com.co>
2024-05-17 10:49:14 -0500

    Modulo Impl. BDs | Intro a JPA + Hibernate - MiniSeries

---
+++ 7. Implementacion de bases de datos/01-jpa-hibernate/01-introduccion-jpa-hibernate/01-miniseries-clase-vivo/src/main/java/org/ggomezr/miniseries/model/MiniSerie.java
@@ -0,0 +1,18 @@
+package org.ggomezr.miniseries.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Double rating;
+    private Integer amount_of_awards;
+}
