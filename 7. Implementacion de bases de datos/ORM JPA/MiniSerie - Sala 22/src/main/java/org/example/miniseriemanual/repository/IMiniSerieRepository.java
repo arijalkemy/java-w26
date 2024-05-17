add6cf5db979032d2698805f5f04d6ea64983aa9
tdonzis_meli <tomas.donzis@mercadolibre.com>
2024-05-17 17:52:33 -0300

    Agrego resolución de sala 22 de miniseries

---
+++ 7. Implementacion de bases de datos/ORM JPA/MiniSerie - Sala 22/src/main/java/org/example/miniseriemanual/repository/IMiniSerieRepository.java
@@ -0,0 +1,9 @@
+package org.example.miniseriemanual.repository;
+
+import org.example.miniseriemanual.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniSerieRepository extends JpaRepository<MiniSerie, Long> {
+}
