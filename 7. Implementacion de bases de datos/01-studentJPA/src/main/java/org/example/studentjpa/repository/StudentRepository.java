94a8cdede08d74b9ef2d5ccbeed9f44044ad0683
cbec <camila.beczkowski@mercadolibre.com>
2024-05-17 15:31:40 -0300

    ej student implementation bd

---
+++ 7. Implementacion de bases de datos/01-studentJPA/src/main/java/org/example/studentjpa/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package org.example.studentjpa.repository;
+
+import org.example.studentjpa.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
---
+++ 7. Implementacion de bases de datos/01-studentJPA/src/main/java/org/example/studentjpa/service/StudentService.java
@@ -0,0 +1,15 @@
+package org.example.studentjpa.service;
+
+import org.example.studentjpa.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    @Autowired
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+}
