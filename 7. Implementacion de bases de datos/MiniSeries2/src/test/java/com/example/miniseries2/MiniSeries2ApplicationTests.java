4beddb22db9bdb72ae07beef0a9928c9589fb5d4
ignruiz <ignacio.iruiz@mercadolibre.com>
2024-05-17 17:50:48 -0300

    ejercicio mini series

---
+++ 7. Implementacion de bases de datos/MiniSeries2/src/test/java/com/example/miniseries2/MiniSeries2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniseries2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSeries2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
