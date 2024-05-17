f7896e61210aa517910d288c0a2a0771772d1063
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-17 15:50:31 -0300

    proyeto de prueba spring h2

---
+++ 7. Implementacion de bases de datos/0. Inicializando H2 con SpringBoot/src/test/java/meli/bootcamp/h2_springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.h2_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
