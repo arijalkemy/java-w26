514201c373ad25e9e79bc6a540993ed1e2c32992
levianfrancisco <levianlastra@gmail.com>
2024-05-17 17:55:16 -0300

    Implementacion de BD con H2 e Hibernate

---
+++ 7. Implementacion de bases de datos/ejercicio_miniseries/src/test/java/com/example/ejercicio_miniseries/EjercicioMiniseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio_miniseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioMiniseriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
