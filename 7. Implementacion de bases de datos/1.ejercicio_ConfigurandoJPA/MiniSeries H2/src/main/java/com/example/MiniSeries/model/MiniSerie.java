1cb10a3e147540d4f3ea42bcd1f49fe7654b8521
abornacelly <aldo.bornacelly@mercadolibre.com.co>
2024-05-18 21:04:04 -0500

    Se agrega la practica de implementacion de JPA

---
+++ 7. Implementacion de bases de datos/1.ejercicio_ConfigurandoJPA/MiniSeries H2/src/main/java/com/example/MiniSeries/model/MiniSerie.java
@@ -0,0 +1,20 @@
+package com.example.MiniSeries.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+
+    private Double rating;
+    @Column( name = "amount_of_awards")
+    private int amountOfAwards;
+}
