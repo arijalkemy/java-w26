11c7b8594a4e78082cf7f94af3a384bda05877d0
Bryann Arlye Torres Rodríguez <164513573+BryannTorresMELI@users.noreply.github.com>
2024-05-20 10:20:07 -0500

    feat: create project for jewelry practice

---
+++ 7. Implementacion de bases de datos/pearl_jewelry/src/main/java/org/example/pearl_jewelry/PearlJewelryApplication.java
@@ -0,0 +1,13 @@
+package org.example.pearl_jewelry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PearlJewelryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PearlJewelryApplication.class, args);
+    }
+
+}
---
+++ 7. Implementacion de bases de datos/pearl_jewelry/src/test/java/org/example/pearl_jewelry/PearlJewelryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pearl_jewelry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PearlJewelryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
