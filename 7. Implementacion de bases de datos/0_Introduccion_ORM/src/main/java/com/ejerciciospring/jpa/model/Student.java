1ee55c464835c878954ffb8518327cff4792a9b6
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-05-19 22:09:41 -0500

    added first jpa exercise

---
+++ 7. Implementacion de bases de datos/0_Introduccion_ORM/src/main/java/com/ejerciciospring/jpa/model/Student.java
@@ -0,0 +1,21 @@
+package com.ejerciciospring.jpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String dni;
+    private String nombre;
+    private String apellido;
+}
