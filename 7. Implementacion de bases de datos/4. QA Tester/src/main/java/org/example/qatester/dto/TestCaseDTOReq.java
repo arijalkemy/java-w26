fb2170e8c4a9756a4139052217214abe06de96d0
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-20 16:14:03 -0300

    style: code format

--- 7. Implementacion de bases de datos/4. QA Tester/src/main/java/org/example/qatester/dto/TestCaseDTOReq.java
+++ 7. Implementacion de bases de datos/4. QA Tester/src/main/java/org/example/qatester/dto/TestCaseDTOReq.java
@@ -1,22 +1,22 @@
 package org.example.qatester.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class TestCaseDTOReq {
-    @JsonProperty("id_case")
-    private Long idCase;
     private String description;
     private Boolean tested;
     private Boolean passed;
+
     @JsonProperty("number_of_tries")
     private int numberOfTries;
+
     @JsonProperty("last_update")
     private LocalDate lastUdate;
 }
--- 7. Implementacion de bases de datos/4. QA Tester/src/main/java/org/example/qatester/dto/TestCaseDTORes.java
+++ 7. Implementacion de bases de datos/4. QA Tester/src/main/java/org/example/qatester/dto/TestCaseDTORes.java
@@ -1,16 +1,25 @@
 package org.example.qatester.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class TestCaseDTORes {
+    @JsonProperty("id_case")
+    private Long idCase;
 
     private String description;
     private Boolean tested;
     private Boolean passed;
+
     @JsonProperty("number_of_tries")
     private int numberOfTries;
+
     @JsonProperty("last_update")
     private LocalDate lastUdate;
 }
