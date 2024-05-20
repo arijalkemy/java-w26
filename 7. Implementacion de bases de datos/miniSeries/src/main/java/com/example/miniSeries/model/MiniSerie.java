d811c4ff98701edfa185fb2e46448c3cc0ae0c6d
Dario Leonel Abatte <dario.abatte@mercadolibre.com>
2024-05-20 10:32:22 -0300

    Se agrega implementacion de JPA con H2

---
+++ 7. Implementacion de bases de datos/miniSeries/src/main/java/com/example/miniSeries/model/MiniSerie.java
@@ -0,0 +1,23 @@
+package com.example.miniSeries.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class MiniSerie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    @SequenceGenerator(name = "seq", sequenceName = "seq", allocationSize = 1)
+    private Long id;
+    private String name;
+    private Double rating;
+    private int amount_of_awards;
+}
---
+++ 7. Implementacion de bases de datos/miniSeries/src/main/java/com/example/miniSeries/repository/MiniSerieRepository.java
@@ -0,0 +1,10 @@
+package com.example.miniSeries.repository;
+
+import com.example.miniSeries.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MiniSerieRepository extends JpaRepository<MiniSerie, Long> {
+
+}
