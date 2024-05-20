f54c68de0edff240b6de61343bdce53075bcdd51
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-20 16:14:52 -0300

    validar campos

--- 7. Implementacion de bases de datos/4. QA Tester/src/main/java/org/example/qatester/dto/TestCaseDTOReq.java
+++ 7. Implementacion de bases de datos/4. QA Tester/src/main/java/org/example/qatester/dto/TestCaseDTOReq.java
@@ -1,6 +1,9 @@
 package org.example.qatester.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +13,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class TestCaseDTOReq {
+    @NotBlank(message = "Description cannot be blank")
     private String description;
+
+    @NotNull(message = "Tested cannot be null")
     private Boolean tested;
+
+    @NotNull(message = "Tested cannot be null")
     private Boolean passed;
 
     @JsonProperty("number_of_tries")
     private int numberOfTries;
 
     @JsonProperty("last_update")
-    private LocalDate lastUdate;
+    @NotNull(message = "Last update cannot be null")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
+    private LocalDate lastUpdate;
 }
