239282984b3fcb1e5896d006cb77708baa44592d
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-05-20 13:50:39 -0500

    Added one to one exercises

---
+++ 7. Implementacion de bases de datos/5_Relaciones_JPA/1_One_To_One/src/main/java/com/ejerciciosjpa/jpaonetoone/JpaOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.ejerciciosjpa.jpaonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOneToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaOneToOneApplication.class, args);
+    }
+
+}
