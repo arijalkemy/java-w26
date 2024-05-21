946726778bdedd4e619cdd621ec4173cd8343b70
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-21 15:37:06 -0300

    agregar proyecto elasticsearch spring data

---
+++ 7. Implementacion de bases de datos/7. elasticsearch-spring/src/main/java/meli/bootcamp/elasticsearchjpa/ElasticsearchJpaApplication.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.elasticsearchjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchJpaApplication.class, args);
+    }
+
+}
---
+++ 7. Implementacion de bases de datos/7. elasticsearch-spring/src/test/java/meli/bootcamp/elasticsearchjpa/ElasticsearchJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.elasticsearchjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
