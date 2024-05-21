4df68ae84658c1f02c8fd0ffdc2c93cccf4880cf
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-21 16:55:55 -0300

    proyecto en vivo elasticsearch springdata

---
+++ 7. Implementacion de bases de datos/8. Elastic Spring Data En vivo/src/main/java/meli/bootcamp/spring_data_elasticsearch/Application.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.spring_data_elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ 7. Implementacion de bases de datos/8. Elastic Spring Data En vivo/src/test/java/meli/bootcamp/spring_data_elasticsearch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.spring_data_elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
