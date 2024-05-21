c7f74960d0f204798f479fd720ff9d4aac56fe59
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-21 16:35:13 -0300

    cambiar nombre directorio

--- 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/repository/IVehiculosRepository.java
+++ 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/repository/IVehiculosRepository.java
@@ -4,4 +4,5 @@ import meli.bootcamp.hql.model.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IVehiculosRepository extends JpaRepository<Vehiculo, Long> {
+
 }
