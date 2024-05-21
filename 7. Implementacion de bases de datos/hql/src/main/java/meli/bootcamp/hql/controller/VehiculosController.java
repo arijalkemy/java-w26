0b0771173578277eea8bb0c677d214d9c8de8e48
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-21 12:27:20 -0300

    agregar controllers vacios

---
+++ 7. Implementacion de bases de datos/hql/src/main/java/meli/bootcamp/hql/controller/VehiculosController.java
@@ -0,0 +1,10 @@
+package meli.bootcamp.hql.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vehiculos")
+public class VehiculosController {
+
+}
