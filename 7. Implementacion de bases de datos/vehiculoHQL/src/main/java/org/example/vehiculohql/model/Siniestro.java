3d8019605351079bcbbb7993157c50a78e254056
Bryann Arlye Torres Rodríguez <164513573+BryannTorresMELI@users.noreply.github.com>
2024-05-21 13:20:36 -0500

    feat: vehicles project with HQL and MySQL

---
+++ 7. Implementacion de bases de datos/vehiculoHQL/src/main/java/org/example/vehiculohql/model/Siniestro.java
@@ -0,0 +1,26 @@
+package org.example.vehiculohql.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Siniestro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private Date fechaSiniestro;
+
+    private double perdidaEconomica;
+
+    @ManyToOne
+    @JoinColumn(name = "vehiculo_id")
+    private Vehiculo vehiculoDenunciado;
+}
