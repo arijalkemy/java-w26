e2684c421ffdc3f6c61d6d8ac45dedeb87584850
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-22 09:33:06 -0300

    validar dto

--- 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/dto/VehiculoResDto.java
+++ 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/dto/VehiculoResDto.java
@@ -1,6 +1,8 @@
 package meli.bootcamp.hql.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,14 +13,24 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VehiculoResDto {
+    @NotNull(message = "El id es requerido")
     private Long id;
+
+    @NotNull(message = "La patente es requerida")
     private String patente;
+
+    @NotNull(message = "La marca es requerida")
     private String marca;
+
+    @NotNull(message = "El modelo es requerido")
     private String modelo;
 
     @JsonProperty("anio_fabricacion")
+    @NotNull(message = "El año de fabricación es requerido")
     private Integer anioFabricacion;
 
     @JsonProperty("cantidad_ruedas")
+    @NotNull(message = "La cantidad de ruedas es requerida")
+    @Min(value = 1, message = "La cantidad de ruedas debe ser mayor a 0")
     private Integer cantidadRuedas;
 }
