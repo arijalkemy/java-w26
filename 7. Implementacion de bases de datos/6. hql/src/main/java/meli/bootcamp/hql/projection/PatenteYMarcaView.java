cea307e3899bee5bac952c6df4dcee84944ef676
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-21 23:37:35 -0300

    agregar queries y projections

---
+++ 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/projection/PatenteYMarcaView.java
@@ -0,0 +1,7 @@
+package meli.bootcamp.hql.projection;
+
+public interface PatenteYMarcaView {
+    String getPatente();
+
+    String getMarca();
+}
--- 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/repository/IVehiculosRepository.java
+++ 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/repository/IVehiculosRepository.java
@@ -1,8 +1,25 @@
 package meli.bootcamp.hql.repository;
 
+import java.util.List;
 import meli.bootcamp.hql.model.Vehiculo;
+import meli.bootcamp.hql.projection.PatenteYMarcaView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IVehiculosRepository extends JpaRepository<Vehiculo, Long> {
 
+    @Query("SELECT v.patente FROM Vehiculo v")
+    List<String> findAllPatentes();
+
+    @Query("SELECT v.patente, v.marca FROM Vehiculo v order by v.anioFabricacion desc")
+    List<PatenteYMarcaView> findAllPatentesYMarcasOrdenadasPorAnioFabricacionDesc();
+
+    @Query("SELECT v.patente, v.marca FROM Vehiculo v order by v.anioFabricacion")
+    List<PatenteYMarcaView> findAllPatentesYMarcasOrdenadasPorAnioFabricacionAsc();
+
+    @Query(
+        "SELECT v.patente FROM Vehiculo v " +
+        "WHERE v.cantidadRuedas > 4 AND v.anioFabricacion = year(CURRENT DATE)"
+    )
+    List<String> findAllPatentesDeMasDeCuatroRuedasFabricadosEsteAnio();
 }
