ee50c7a291dd06825bd78005145411ddf2c0b872
valentin_aquino <valentin.aquino@mercadolibre.com>
2024-05-22 00:36:31 -0300

    fix query para que los campos coincidan con la projection

--- 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/repository/IVehiculosRepository.java
+++ 7. Implementacion de bases de datos/6. hql/src/main/java/meli/bootcamp/hql/repository/IVehiculosRepository.java
@@ -11,15 +11,19 @@ public interface IVehiculosRepository extends JpaRepository<Vehiculo, Long> {
     @Query("SELECT v.patente FROM Vehiculo v")
     List<String> findAllPatentes();
 
-    @Query("SELECT v.patente, v.marca FROM Vehiculo v order by v.anioFabricacion desc")
+    @Query(
+        "SELECT v.patente as patente, v.marca as marca FROM Vehiculo v " +
+            "ORDER BY v.anioFabricacion desc")
     List<PatenteYMarcaView> findAllPatentesYMarcasOrdenadasPorAnioFabricacionDesc();
 
-    @Query("SELECT v.patente, v.marca FROM Vehiculo v order by v.anioFabricacion")
+    @Query(
+        "SELECT v.patente as patente, v.marca as marca FROM Vehiculo v " +
+            "ORDER BY v.anioFabricacion")
     List<PatenteYMarcaView> findAllPatentesYMarcasOrdenadasPorAnioFabricacionAsc();
 
     @Query(
         "SELECT v.patente FROM Vehiculo v " +
-        "WHERE v.cantidadRuedas > 4 AND v.anioFabricacion = year(CURRENT DATE)"
+            "WHERE v.cantidadRuedas > 4 AND v.anioFabricacion = year(CURRENT DATE)"
     )
     List<String> findAllPatentesDeMasDeCuatroRuedasFabricadosEsteAnio();
 }
