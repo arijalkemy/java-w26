3680692dfcc41bc8501ad1535b0fa79268c3bb3c
fabian001254 <facatrusa@gmail.com>
2024-05-22 14:18:36 -0500

    Ejercicio con mysql y elasticseaarch

--- 7. Implementacion de bases de datos/CrudTestJPA/crudjpa/src/main/java/com/meli/crudjpa/repository/JoyaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meli.crudjpa.repository;
-
-import com.meli.crudjpa.model.Joya;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JoyaRepository extends JpaRepository<Joya, Integer> {
-}
