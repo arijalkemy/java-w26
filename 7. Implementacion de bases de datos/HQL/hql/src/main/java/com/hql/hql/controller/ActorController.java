7cf4ed8289457026850ffa434f76437c0148ac53
Lautaro Oleastro <lautaro.oleastro@mercadolibre.com>
2024-05-23 14:55:19 -0300

    MoviesDB HQL. Se arreglo name de un metodo del controller. Pendientes algunos endpoints.

--- 7. Implementacion de bases de datos/HQL/hql/src/main/java/com/hql/hql/controller/ActorController.java
+++ 7. Implementacion de bases de datos/HQL/hql/src/main/java/com/hql/hql/controller/ActorController.java
@@ -2,30 +2,32 @@ package com.hql.hql.controller;
 
 import com.hql.hql.model.Actor;
 import com.hql.hql.service.IActorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/movies/actor")
 public class ActorController {
 
-    @Autowired
-    IActorService actorService;
+
+    private final IActorService actorService;
 
     @GetMapping("/{rating}")
     public ArrayList<Actor> getActorsGreaterThanRating(@PathVariable int rating) {
         return actorService.findActorByRatingGreaterThan(rating);
     }
 
-    @GetMapping()
+    @GetMapping("/with-favourite-movie")
     public ArrayList<Actor> getActorsWithFavouriteMovie() {
         return actorService.findActorByFavoriteMovieNotNull();
     }
 
     @GetMapping("name/{title}")
-    public ArrayList<Actor> getActorsGreaterThanRating(@PathVariable String title) {
+    public ArrayList<Actor> getActorsFeaturedInMovieWithName(@PathVariable String title) {
         return actorService.findActorByMovieName(title);
     }
 }
