9b9e7531a567935a3fcef3524eee00030fb08693
marcosdittaML <marcos.ditta@mercadolibre.com>
2024-05-26 17:35:07 -0300

    Subo ejercicio de practica implementando jpa

---
+++ 7. Implementacion de bases de datos/JPA + Hibernate/implBD-MiniSeries/src/main/java/com/example/implbdminiseries/ImplBdMiniSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.implbdminiseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplBdMiniSeriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplBdMiniSeriesApplication.class, args);
+    }
+
+}
---
+++ 7. Implementacion de bases de datos/JPA + Hibernate/implBD-MiniSeries/src/test/java/com/example/implbdminiseries/ImplBdMiniSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.implbdminiseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplBdMiniSeriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
