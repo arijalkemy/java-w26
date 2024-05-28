520b605217ba6c6963002571d85eb7e64e1f9b8e
frmoises_meli <franco.fmoises@mercadolibre.com>
2024-05-28 15:16:59 -0300

    Agrego ejercicio JPAImplementacion

---
+++ 7. Implementacion de bases de datos/JPAImplementation/src/main/java/com/bootcamp/JPAImplementation/entity/Course.java
@@ -0,0 +1,34 @@
+package com.bootcamp.JPAImplementation.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id")
+    private Teacher teacherOnCharge;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "course_mentors",
+    joinColumns = @JoinColumn(name = "course_id"),
+    inverseJoinColumns = @JoinColumn(name = "mentor_id"))
+    private List<Mentor> mentors;
+    /*@ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "student_courses",
+    joinColumns = @JoinColumn(name = "course_id"),
+    inverseJoinColumns = @JoinColumn(name = "student_id"))*/
+    /*@OneToMany(mappedBy = "student")
+    private List<Student> students;*/
+
+}
