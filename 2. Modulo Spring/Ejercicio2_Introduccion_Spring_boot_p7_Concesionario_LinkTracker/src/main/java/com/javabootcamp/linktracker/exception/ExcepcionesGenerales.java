7f1a2da61c9ee9638aeb5df3831da17f57e3b664
ctorreschapa <cristianandres.torreschaparro@mercadolibre.com.co>
2024-06-02 21:20:59 -0500

    Add files via upload

---
+++ 2. Modulo Spring/Ejercicio2_Introduccion_Spring_boot_p7_Concesionario_LinkTracker/src/main/java/com/javabootcamp/linktracker/exception/ExcepcionesGenerales.java
@@ -0,0 +1,9 @@
+package com.javabootcamp.linktracker.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice(annotations = RestController.class)
+public class ExcepcionesGenerales {
+
+}
---
+++ 2. Modulo Spring/Ejercicio2_Introduccion_Spring_boot_p7_Concesionario_LinkTracker/src/main/java/com/javabootcamp/linktracker/model/DTO/MensajeDTO.java
@@ -0,0 +1,10 @@
+package com.javabootcamp.linktracker.model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MensajeDTO {
+    private String mensajeDelServidor;
+}
