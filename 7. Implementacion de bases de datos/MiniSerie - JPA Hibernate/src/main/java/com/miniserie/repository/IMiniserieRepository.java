814e3ea49e85e1a13af24c8e1b15a9245e6186f3
FranciscoCardosoRodriguez <franciscocardosorodriguez@gmail.com>
2024-06-21 00:57:15 -0600

    ADD: MiniSerie

---
+++ 7. Implementacion de bases de datos/MiniSerie - JPA Hibernate/src/main/java/com/miniserie/repository/IMiniserieRepository.java
@@ -0,0 +1,9 @@
+package com.miniserie.repository;
+
+import com.miniserie.entity.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniserieRepository extends JpaRepository<MiniSerie, Long> {
+}
