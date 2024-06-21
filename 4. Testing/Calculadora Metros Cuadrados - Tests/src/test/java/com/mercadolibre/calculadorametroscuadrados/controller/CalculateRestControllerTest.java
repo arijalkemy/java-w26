e709f534e27f9b2c8edf8d7d408f781b4fe6d422
FranciscoCardosoRodriguez <franciscocardosorodriguez@gmail.com>
2024-06-21 00:06:47 -0600

    ADD: Ejercicio de Calculadora

---
+++ 4. Testing/Calculadora Metros Cuadrados - Tests/src/test/java/com/mercadolibre/calculadorametroscuadrados/controller/CalculateRestControllerTest.java
@@ -0,0 +1,85 @@
+package com.mercadolibre.calculadorametroscuadrados.controller;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseResponseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.RoomDTO;
+import com.mercadolibre.calculadorametroscuadrados.service.CalculateService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class CalculateRestControllerTest {
+    @Mock
+    CalculateService calculateService;
+
+    @InjectMocks
+    CalculateRestController controller;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void calculate() throws JsonProcessingException {
+// Crear objetos RoomDTO
+        RoomDTO room1 = new RoomDTO();
+        room1.setName("Espacio abierto");
+        room1.setWidth(5);
+        room1.setLength(5);
+
+        RoomDTO room2 = new RoomDTO();
+        room2.setName("Cocina");
+        room2.setWidth(3);
+        room2.setLength(3);
+
+        RoomDTO room3 = new RoomDTO();
+        room3.setName("Baño");
+        room3.setWidth(2);
+        room3.setLength(1);
+
+// Crear una lista de objetos RoomDTO
+        List<RoomDTO> rooms = Arrays.asList(room1, room2, room3);
+
+// Crear un objeto HouseDTO
+        HouseDTO house = new HouseDTO();
+        house.setName("Oficina");
+        house.setAddress("Monroe 800");
+        house.setRooms(rooms);
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+
+        HouseResponseDTO response = new HouseResponseDTO(house);
+        response.setPrice(28800);
+        response.setSquareFeet(36);
+        response.setBiggest(room1);
+
+
+        // act
+        when(this.calculateService.calculate(house)).thenReturn(response);
+        HouseResponseDTO result = this.controller.calculate(house);
+
+        // assert
+        String payloadJSON = writer.writeValueAsString(response);
+        String responseJSON = writer.writeValueAsString(result);
+
+        assertEquals(payloadJSON, responseJSON);
+
+    }
+
+}
